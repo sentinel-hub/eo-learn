181e33242226dcef43f72067de57260a74fbc8ec
Devis Peressutti <devis.peressutti@sinergise.com>
2018-10-01 12:40:38 +0200

    refactor blob tasks to ease initialisation
     * create base BlobTask with implementation
     * create three derived classes (DoGBlobTask, LoGBlobTask, DoHBlobTask) with different arguments
     * implement basic unittests

--- features/eolearn/features/blob.py
+++ features/eolearn/features/blob.py
@@ -1,8 +1,8 @@
 """ Module for computing blobs in EOPatch """
-
-import numpy as np
-from math import sqrt
+from skimage.feature import blob_dog, blob_doh, blob_log
 from eolearn.core import EOTask, FeatureType
+from math import sqrt
+import numpy as np
 
 
 class BlobTask(EOTask):
@@ -19,136 +19,88 @@ class BlobTask(EOTask):
     ie : If blob[date, i, j, 0] = 5 then a blob of radius 5 is present at he coordinate (i, j)
 
     The task uses skimage.feature.blob_log or skimage.feature.blob_dog or skimage.feature.blob_doh to extract the blobs.
+
+    The input image must be in [-1,1] range.
+
+    :param feature: A feature that will be used and a new feature name where data will be saved. If new name is not
+                    specified it will be saved with name '<feature_name>_BLOB'
+
+                    Example: (FeatureType.DATA, 'bands') or (FeatureType.DATA, 'bands', 'blob')
+
+    :type feature: (FeatureType, str) or (FeatureType, str, str)
+    :param blob_object: Name of the blob method to use
+    :type blob_object: skimage.features.blob_*
+    :param blob_parameters: List of parameters to be passed to the blob function. Below a list of such parameters.
+    :type blob_paramters: dict
+    :param min_sigma: The minimum standard deviation for Gaussian Kernel. Keep this low to detect smaller blobs
+    :type min_sigma: float
+    :param max_sigma: The maximum standard deviation for Gaussian Kernel. Keep this high to detect larger blobs
+    :type float
+    :param threshold: The absolute lower bound for scale space maxima. Local maxima smaller than thresh are ignored.
+                        Reduce this to detect blobs with less intensity
+    :type threshold: float
+    :param overlap: A value between 0 and 1. If the area of two blobs overlaps by a fraction greater than threshold,
+                    the smaller blob is eliminated
+    :type overlap: float
+    :param num_sigma: For ‘Log’ and ‘DoH’: The number of intermediate values of standard deviations to consider between
+                        min_sigma and max_sigma
+    :type num_sigma: int
+    :param log_scale: For ‘Log’ and ‘DoH’: If set intermediate values of standard deviations are interpolated using a
+                        logarithmic scale to the base 10. If not, linear interpolation is used
+    :type log_scale: bool
+    :param sigma_ratio: For ‘DoG’: The ratio between the standard deviation of Gaussian Kernels used for computing the
+                        Difference of Gaussians
+    :type sigma_ratio: float
     """
-    AVAILABLE_METHODS = {
-        'LoG',
-        'DoG',
-        'DoH'
-    }
 
-    def __init__(self, feature, method, min_sigma=1, max_sigma=30, threshold=0.1, overlap=0.5, **kwargs):
-        """
-        :param feature: A feature that will be used and a new feature name where data will be saved. If new name is not
-        specified it will be saved with name '<feature_name>_HARALICK'
-
-        Example: (FeatureType.DATA, 'bands') or (FeatureType.DATA, 'bands', 'blob')
-
-        :type feature: (FeatureType, str) or (FeatureType, str, str)
-        :param method: Name of the method to use : Must be one of these : ‘Log’, ‘DoG’ or ‘DoH’
-        :type method: str
-        :param min_sigma: The minimum standard deviation for Gaussian Kernel. Keep this low to detect smaller blobs
-        :type min_sigma: float
-        :param max_sigma: The maximum standard deviation for Gaussian Kernel. Keep this high to detect larger blobs
-        :type float
-        :param threshold: The absolute lower bound for scale space maxima. Local maxima smaller than thresh are ignored.
-        Reduce this to detect blobs with less intensity
-        :type threshold: float
-        :param overlap: A value between 0 and 1. If the area of two blobs overlaps by a fraction greater than threshold,
-        the smaller blob is eliminated
-        :type overlap: float
-        :param kwargs: Depending of the method other arguments are available:
-        For ‘Log’ and ‘DoH’:
-        :param num_sigma: The number of intermediate values of standard deviations to consider between min_sigma and
-        max_sigma
-        :type num_sigma: int
-        :param log_scale: If set intermediate values of standard deviations are interpolated using a logarithmic scale
-        to the base 10. If not, linear interpolation is used
-        :type log_scale: bool
-        For ‘DoG’:
-        :param sigma_ratio: The ratio between the standard deviation of Gaussian Kernels used for computing the
-        Difference of Gaussians
-        :type max_sigma: float
-        """
+    def __init__(self, feature, blob_object, **blob_parameters):
         self.feature = self._parse_features(feature, default_feature_type=FeatureType.DATA, new_names=True,
                                             rename_function='{}_BLOB'.format)
 
-        self.min_sigma = min_sigma
-        self.max_sigma = max_sigma
-        self.threshold = threshold
-        self.overlap = overlap
-        self.blob_method = None
-        self.method = method
-
-        # Read arguments depending on the chosen method
-        if self.method == 'LoG':
-            from skimage.feature import blob_log
-            self.blob_method = blob_log
-            self.num_sigma = 10
-            self.log_scale = False
-            for key, value in kwargs.items():
-                if key == 'num_sigma':
-                    self.num_sigma = value
-                elif key == 'log_scale':
-                    self.log_scale = value
-            self.param = {
-                'min_sigma': self.min_sigma,
-                'max_sigma': self.max_sigma,
-                'num_sigma': self.num_sigma,
-                'threshold': self.threshold,
-                'overlap': self.overlap,
-                'log_scale': self.log_scale
-            }
-
-        elif self.method == 'DoG':
-            from skimage.feature import blob_dog
-            self.blob_method = blob_dog
-            self.sigma_ratio = 1.6
-            for key, value in kwargs.items():
-                if key == 'sigma_ratio':
-                    self.sigma_ratio = value
-            self.param = {
-                'min_sigma': self.min_sigma,
-                'max_sigma': self.max_sigma,
-                'sigma_ratio': self.sigma_ratio,
-                'threshold': self.threshold,
-                'overlap': self.overlap
-            }
-
-        elif self.method == 'DoH':
-            from skimage.feature import blob_doh
-            self.blob_method = blob_doh
-            self.num_sigma = 10
-            self.log_scale = False
-            for key, value in kwargs.items():
-                if key == 'num_sigma':
-                    self.num_sigma = value
-                elif key == 'log_scale':
-                    self.log_scale = value
-            self.param = {
-                'min_sigma': self.min_sigma,
-                'max_sigma': self.max_sigma,
-                'num_sigma': self.num_sigma,
-                'threshold': self.threshold,
-                'overlap': self.overlap,
-                'log_scale': self.log_scale
-            }
-            self.param = {
-                'min_sigma': self.min_sigma,
-                'max_sigma': self.max_sigma,
-                'num_sigma': self.num_sigma,
-                'threshold': self.threshold,
-                'overlap': self.overlap,
-                'log_scale': self.log_scale
-            }
-        else:
-            raise ValueError('Blob method must be one of these : {}'.format(self.AVAILABLE_METHODS))
+        self.blob_object = blob_object
+        self.blob_parameters = blob_parameters
 
     def _compute_blob(self, data):
         result = np.zeros(data.shape, dtype=np.float)
         for time in range(data.shape[0]):
             for band in range(data.shape[3]):
                 image = data[time, :, :, band]
-                res = np.asarray(self.blob_method(image, **self.param))
+                res = np.asarray(self.blob_object(image, **self.blob_parameters))
                 x_coord = res[:, 0].astype(np.int)
                 y_coord = res[:, 1].astype(np.int)
                 radius = res[:, 2] * sqrt(2)
-                for i in range(radius.shape[0]):
-                    result[time, x_coord[i], y_coord[i], band] = radius[i]
+                result[time, x_coord, y_coord, band] = radius
         return result
 
     def execute(self, eopatch):
+        """ Execute computation of blobs on input eopatch
 
+        :param eopatch: Input eopatch
+        :type eopatch: eolearn.core.EOPatch
+        :return: EOPatch instance with new key holding the blob image.
+        :rtype: eolearn.core.EOPatch
+        """
         for feature_type, feature_name, new_feature_name in self.feature:
             eopatch[feature_type][new_feature_name] = self._compute_blob(eopatch[feature_type][feature_name])
 
         return eopatch
+
+
+class DoGBlobTask(BlobTask):
+    def __init__(self, feature, *, sigma_ratio=1.6, min_sigma=1, max_sigma=30, threshold=0.1, overlap=0.5, **kwargs):
+        super().__init__(feature, blob_dog, sigma_ratio=sigma_ratio, min_sigma=min_sigma, max_sigma=max_sigma,
+                         threshold=threshold, overlap=overlap, **kwargs)
+
+
+class DoHBlobTask(BlobTask):
+    def __init__(self, feature, *, num_sigma=10, log_scale=False, min_sigma=1, max_sigma=30, threshold=0.1, overlap=0.5,
+                 **kwargs):
+        super().__init__(feature, blob_doh, num_sigma=num_sigma, log_scale=log_scale, min_sigma=min_sigma,
+                         max_sigma=max_sigma, threshold=threshold, overlap=overlap, **kwargs)
+
+
+class LoGBlobTask(BlobTask):
+    def __init__(self, feature, *, num_sigma=10, log_scale=False, min_sigma=1, max_sigma=30, threshold=0.1, overlap=0.5,
+                 **kwargs):
+        super().__init__(feature, blob_log, num_sigma=num_sigma, log_scale=log_scale, min_sigma=min_sigma,
+                         max_sigma=max_sigma, threshold=threshold, overlap=overlap, **kwargs)
--- features/eolearn/tests/test_blob.py
+++ features/eolearn/tests/test_blob.py
@@ -1,10 +1,10 @@
-
 import unittest
 import os.path
 import numpy as np
+from skimage.feature import blob_dog
 
 from eolearn.core import EOPatch, FeatureType
-from eolearn.features import BlobTask
+from eolearn.features import BlobTask, DoGBlobTask, LoGBlobTask, DoHBlobTask
 
 
 class TestBlob(unittest.TestCase):
@@ -16,8 +16,11 @@ class TestBlob(unittest.TestCase):
         cls.patch = EOPatch.load(cls.TEST_PATCH_FILENAME)
         cls._prepare_patch(cls.patch)
 
-        BlobTask((FeatureType.DATA, 'ndvi', 'blob'), method='DoG', min_sigma=1, max_sigma=30,
-                 threshold=0, overlap=0.5).execute(cls.patch)
+        BlobTask((FeatureType.DATA, 'ndvi', 'blob'), blob_dog, sigma_ratio=1.6, min_sigma=1, max_sigma=30,
+                 overlap=0.5, threshold=0).execute(cls.patch)
+        DoGBlobTask((FeatureType.DATA, 'ndvi', 'blob_dog'), threshold=0).execute(cls.patch)
+        LoGBlobTask((FeatureType.DATA, 'ndvi', 'blob_log'), log_scale=True, threshold=0).execute(cls.patch)
+        DoHBlobTask((FeatureType.DATA, 'ndvi', 'blob_doh'), num_sigma=5, threshold=0).execute(cls.patch)
 
         cls.initial_patch = EOPatch.load(cls.TEST_PATCH_FILENAME)
         cls._prepare_patch(cls.initial_patch)
@@ -28,8 +31,12 @@ class TestBlob(unittest.TestCase):
         ndvi[np.isnan(ndvi)] = 0
         patch.data['ndvi'] = ndvi
 
-    def test_new_feature(self):
-        blob = self.patch.data['blob']
+    def test_blob_feature(self):
+        self.assertTrue(np.allclose(self.patch.data['blob'], self.patch.data['blob_dog']),
+                        msg='DoG derived class result not equal to base class result')
+
+    def test_dog_feature(self):
+        blob = self.patch.data['blob_dog']
         delta = 1e-4
 
         test_min = np.min(blob)
@@ -42,6 +49,50 @@ class TestBlob(unittest.TestCase):
 
         test_mean = np.mean(blob)
         exp_mean = 0.0545
+        self.assertAlmostEqual(test_mean, exp_mean, delta=delta,
+                               msg="Expected mean {}, got {}".format(exp_mean, test_mean))
+
+        test_median = np.median(blob)
+        exp_median = 0.0
+        self.assertAlmostEqual(test_median, exp_median, delta=delta,
+                               msg="Expected median {}, got {}".format(exp_median, test_median))
+
+    def test_log_feature(self):
+        blob = self.patch.data['blob_log']
+        delta = 1e-4
+
+        test_min = np.min(blob)
+        exp_min = 0.0
+        self.assertAlmostEqual(test_min, exp_min, delta=delta, msg="Expected min {}, got {}".format(exp_min, test_min))
+
+        test_max = np.max(blob)
+        exp_max = 13.65408
+        self.assertAlmostEqual(test_max, exp_max, delta=delta, msg="Expected max {}, got {}".format(exp_max, test_max))
+
+        test_mean = np.mean(blob)
+        exp_mean = 0.05728
+        self.assertAlmostEqual(test_mean, exp_mean, delta=delta,
+                               msg="Expected mean {}, got {}".format(exp_mean, test_mean))
+
+        test_median = np.median(blob)
+        exp_median = 0.0
+        self.assertAlmostEqual(test_median, exp_median, delta=delta,
+                               msg="Expected median {}, got {}".format(exp_median, test_median))
+
+    def test_doh_feature(self):
+        blob = self.patch.data['blob_doh']
+        delta = 1e-4
+
+        test_min = np.min(blob)
+        exp_min = 0.0
+        self.assertAlmostEqual(test_min, exp_min, delta=delta, msg="Expected min {}, got {}".format(exp_min, test_min))
+
+        test_max = np.max(blob)
+        exp_max = 1.4142
+        self.assertAlmostEqual(test_max, exp_max, delta=delta, msg="Expected max {}, got {}".format(exp_max, test_max))
+
+        test_mean = np.mean(blob)
+        exp_mean = 0.0007
         self.assertAlmostEqual(test_mean, exp_mean, delta=delta,
                                msg="Expected mean {}, got {}".format(exp_mean, test_mean))
 
