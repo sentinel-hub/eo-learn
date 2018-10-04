e729f4a5e4dd22f8da6cf27c072ba2e3ca95a7f0
Matic Lubej <lubej.matic@gmail.com>
2018-10-04 16:21:27 +0200

    add masks to unit tests, update unit tests

--- features/eolearn/tests/test_interpolation.py
+++ features/eolearn/tests/test_interpolation.py
@@ -4,7 +4,7 @@ import os.path
 import numpy as np
 from datetime import datetime
 
-from eolearn.core import EOPatch
+from eolearn.core import EOPatch, FeatureType
 from eolearn.features import LinearInterpolation, CubicInterpolation, SplineInterpolation, BSplineInterpolation, \
     AkimaInterpolation, LinearResampling, CubicResampling, NearestResampling
 
@@ -15,7 +15,8 @@ class TestInterpolation(unittest.TestCase):
         """
         Container for each interpolation test case
         """
-        TEST_PATCH_FILENAME = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'TestInputs', 'TestPatch')
+        TEST_PATCH_FILENAME = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../../../example_data',
+                                           'TestEOPatch')
 
         def __init__(self, name, task, result_len, img_min=None, img_max=None, img_mean=None, img_median=None,
                      nan_replace=None):
@@ -32,52 +33,68 @@ class TestInterpolation(unittest.TestCase):
 
         def execute(self):
             patch = EOPatch.load(self.TEST_PATCH_FILENAME)
+            self.result = self.task.execute(patch)
+            patch = self.result
+
             if self.nan_replace is not None:
                 feature_type, feature_name, _ = next(self.task.feature(patch))
                 patch[feature_type][feature_name][np.isnan(patch[feature_type][feature_name])] = self.nan_replace
-            self.result = self.task.execute(patch)
 
     @classmethod
     def setUpClass(cls):
 
         cls.test_cases = [
-            cls.InterpolationTestCase('linear', LinearInterpolation('ndvi', result_interval=(-0.3, -0.1),
+            cls.InterpolationTestCase('linear', LinearInterpolation('NDVI', result_interval=(0.0, 1.0),
+                                                                    mask_feature=(FeatureType.MASK, 'IS_VALID'),
                                                                     unknown_value=10),
-                                      result_len=180, img_min=-0.3000, img_max=10.0, img_mean=-0.147169,
-                                      img_median=-0.21132),
-            cls.InterpolationTestCase('cubic', CubicInterpolation('ndvi', result_interval=(-0.3, -0.1),
+                                      result_len=68, img_min=0.0, img_max=10.0, img_mean=0.720405,
+                                      img_median=0.59765935),
+
+            cls.InterpolationTestCase('cubic', CubicInterpolation('NDVI', result_interval=(0.0, 1.0),
+                                                                  mask_feature=(FeatureType.MASK, 'IS_VALID'),
                                                                   resample_range=('2015-01-01', '2018-01-01', 16),
-                                                                  unknown_value=5),
-                                      result_len=69, img_min=-0.3000, img_max=5.0, img_mean=1.30387,
-                                      img_median=-0.1007357),
-            cls.InterpolationTestCase('spline', SplineInterpolation('ndvi', result_interval=(-10, 10),
+                                                                  unknown_value=5, bounds_error=False),
+                                      result_len=69, img_min=0.0, img_max=5.0, img_mean=1.3592644,
+                                      img_median=0.6174331),
+
+            cls.InterpolationTestCase('spline', SplineInterpolation('NDVI', result_interval=(-0.3, 1.0),
+                                                                    mask_feature=(FeatureType.MASK, 'IS_VALID'),
                                                                     resample_range=('2016-01-01', '2018-01-01', 5),
-                                                                    spline_degree=3, smoothing_factor=0),
-                                      result_len=147, img_min=-0.68187547, img_max=0.26355705, img_mean=-0.1886001,
-                                      img_median=-0.18121514),
-            cls.InterpolationTestCase('bspline', BSplineInterpolation('ndvi', unknown_value=-3,
+                                                                    spline_degree=3, smoothing_factor=0,
+                                                                    unknown_value=0),
+                                      result_len=147, img_min=-0.3, img_max=1.0, img_mean=0.492752,
+                                      img_median=0.53776133),
+
+            cls.InterpolationTestCase('bspline', BSplineInterpolation('NDVI', unknown_value=-3,
+                                                                      mask_feature=(FeatureType.MASK, 'IS_VALID'),
                                                                       resample_range=('2017-01-01', '2017-02-01', 50),
                                                                       spline_degree=5),
-                                      result_len=1, img_min=-0.20366311, img_max=0.035470814, img_mean=-0.04307341,
-                                      img_median=-0.040708482),
-            cls.InterpolationTestCase('akima', AkimaInterpolation('ndvi', unknown_value=0),
-                                      result_len=180, img_min=-0.4821199, img_max=0.2299331, img_mean=-0.20141865,
-                                      img_median=-0.213559),
-            cls.InterpolationTestCase('nearest resample', NearestResampling('ndvi', result_interval=(-0.3, -0.1),
+                                      result_len=1, img_min=-0.032482587, img_max=0.701796, img_mean=0.42080238,
+                                      img_median=0.42889267),
+
+            cls.InterpolationTestCase('akima', AkimaInterpolation('NDVI', unknown_value=0,
+                                                                  mask_feature=(FeatureType.MASK, 'IS_VALID')),
+                                      result_len=68, img_min=-0.13793105, img_max=0.860242, img_mean=0.53159297,
+                                      img_median=0.59087014),
+
+            cls.InterpolationTestCase('nearest resample', NearestResampling('NDVI', result_interval=(0.0, 1.0),
                                                                             resample_range=('2016-01-01',
                                                                                             '2018-01-01', 5)),
-                                      result_len=147, img_min=-0.3000, img_max=-0.1000, img_mean=-0.19651729,
-                                      img_median=-0.20000, nan_replace=-0.2),
-            cls.InterpolationTestCase('linear resample', LinearResampling('ndvi', result_interval=(-0.3, -0.1),
+                                      result_len=147, img_min=-0.2, img_max=0.860242, img_mean=0.35143828,
+                                      img_median=0.37481314, nan_replace=-0.2),
+
+            cls.InterpolationTestCase('linear resample', LinearResampling('NDVI', result_interval=(0.0, 1.0),
                                                                           resample_range=('2016-01-01',
                                                                                           '2018-01-01', 5)),
-                                      result_len=147, img_min=-0.3000, img_max=-0.1000, img_mean=-0.1946315,
-                                      img_median=-0.20000, nan_replace=-0.2),
-            cls.InterpolationTestCase('cubic resample', CubicResampling('ndvi', result_interval=(-0.3, -0.1),
+                                      result_len=147, img_min=-0.2, img_max=0.8480114, img_mean=0.350186,
+                                      img_median=0.3393997, nan_replace=-0.2),
+
+            cls.InterpolationTestCase('cubic resample', CubicResampling('NDVI', result_interval=(-0.2, 1.0),
                                                                         resample_range=('2015-01-01', '2018-01-01', 16),
                                                                         unknown_value=5),
-                                      result_len=69, img_min=-0.3000, img_max=5.0, img_mean=1.22359,
-                                      img_median=-0.181167, nan_replace=-0.2)
+                                      result_len=69, img_min=-0.2, img_max=5.0, img_mean=1.2348738,
+                                      img_median=0.4656453, nan_replace=-0.2)
+
         ]
 
         for test_case in cls.test_cases:
@@ -92,8 +109,8 @@ class TestInterpolation(unittest.TestCase):
                 ts_len = len(test_case.result.timestamp)
                 self.assertEqual(ts_len, test_case.result_len,
                                  msg="Expected timestamp len {}, got {}".format(test_case.result_len, ts_len))
-                data_shape = test_case.result.data['ndvi'].shape
-                exp_shape = (test_case.result_len, 20, 20, 1)
+                data_shape = test_case.result.data['NDVI'].shape
+                exp_shape = (test_case.result_len, 101, 100, 1)
                 self.assertEqual(data_shape, exp_shape,
                                  "Expected data shape {}, got {}".format(exp_shape, data_shape))
 
@@ -102,22 +119,22 @@ class TestInterpolation(unittest.TestCase):
             delta = 1e-3
 
             if test_case.img_min is not None:
-                min_val = np.amin(test_case.result.data['ndvi'])
+                min_val = np.amin(test_case.result.data['NDVI'])
                 with self.subTest(msg='Test case {}'.format(test_case.name)):
                         self.assertAlmostEqual(test_case.img_min, min_val, delta=delta,
                                                msg="Expected min {}, got {}".format(test_case.img_min, min_val))
             if test_case.img_max is not None:
-                max_val = np.amax(test_case.result.data['ndvi'])
+                max_val = np.amax(test_case.result.data['NDVI'])
                 with self.subTest(msg='Test case {}'.format(test_case.name)):
                         self.assertAlmostEqual(test_case.img_max, max_val, delta=delta,
                                                msg="Expected max {}, got {}".format(test_case.img_max, max_val))
             if test_case.img_mean is not None:
-                mean_val = np.mean(test_case.result.data['ndvi'])
+                mean_val = np.mean(test_case.result.data['NDVI'])
                 with self.subTest(msg='Test case {}'.format(test_case.name)):
                         self.assertAlmostEqual(test_case.img_mean, mean_val, delta=delta,
                                                msg="Expected mean {}, got {}".format(test_case.img_mean, mean_val))
             if test_case.img_median is not None:
-                median_val = np.median(test_case.result.data['ndvi'])
+                median_val = np.median(test_case.result.data['NDVI'])
                 with self.subTest(msg='Test case {}'.format(test_case.name)):
                         self.assertAlmostEqual(test_case.img_median, median_val, delta=delta,
                                                msg="Expected median {}, got {}".format(test_case.img_median,
