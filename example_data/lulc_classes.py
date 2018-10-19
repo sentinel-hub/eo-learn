4c9d4e8e11e05874d65b128698ee397fe5202d0e
Matic Lubej <lubej.matic@gmail.com>
2018-10-19 13:08:44 +0200

    adding full SI LULC pipeline example + fixing other examples

---
+++ example_data/lulc_classes.py
@@ -0,0 +1,11 @@
+lulc_classes = {'no data':0,
+		'cultivated land':1,
+  		'forest':2,
+		'grassland':3,
+		'shrubland':4,
+		'water':5,
+		'wetlands':6,
+		'tundra':7,
+		'artificial surface':8,
+		'bareland':9,
+		'snow and ice':10}
