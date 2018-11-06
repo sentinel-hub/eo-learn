6bd1987bf18e52fceb45553958a02069ce3ce80c
eerzin <eva.erzin@sinergise.com>
2018-11-06 13:22:05 +0100

    Fixed bug

--- io/eolearn/io/sentinelhub_service.py
+++ io/eolearn/io/sentinelhub_service.py
@@ -157,8 +157,10 @@ class SentinelHubOGCInput(EOTask):
         for param, eoparam in zip(['time', 'time_difference', 'maxcc'], ['time_interval', 'time_difference', 'maxcc']):
             if eoparam not in eopatch.meta_info:
                 if param == 'time':
-                    eopatch.meta_info[eoparam] = [iso_to_datetime(parse_time(x)) if isinstance(x, str)
-                                                  else x for x in request_params[param]]
+                    value = request_params[param]
+                    eopatch.meta_info[eoparam] = value if isinstance(value, str) else [iso_to_datetime(parse_time(x)) if isinstance(x, str)
+                                                  else x for x in value]
+
                 else:
                     eopatch.meta_info[eoparam] = request_params[param]
 
