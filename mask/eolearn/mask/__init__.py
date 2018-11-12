6e40687a841564d7834892b3eb58e09a20915327
JohannesSchmid <hannes_schmid2@gmx.de>
2018-11-12 09:09:34 +0100

    add SCLmask

--- mask/eolearn/mask/__init__.py
+++ mask/eolearn/mask/__init__.py
@@ -1,5 +1,5 @@
 from .cloud_mask import AddCloudMaskTask, get_s2_pixel_cloud_detector
-from .masking import AddValidDataMaskTask
+from .masking import AddValidDataMaskTask, SCLmask
 
 
 __version__ = '0.3.1'
