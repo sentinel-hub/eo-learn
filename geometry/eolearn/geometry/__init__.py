08065ebd9cb6d77dcabf2312f53ed356d4f4750b
Matic Lubej <lubej.matic@gmail.com>
2018-11-14 15:30:21 +0100

    Extracting the erosion task as a standalone task, splitting the unit test and updating to the test eopatch

--- geometry/eolearn/geometry/__init__.py
+++ geometry/eolearn/geometry/__init__.py
@@ -2,7 +2,7 @@
 Subpackage containing EOTasks for geometrical transformations
 """
 
-from .utilities import VectorToRaster
+from .utilities import VectorToRaster, ErosionTask
 from .sampling import PointSamplingTask, PointSampler, PointRasterSampler
 
 __version__ = '0.3.3'
--- geometry/eolearn/geometry/utilities.py
+++ geometry/eolearn/geometry/utilities.py
@@ -5,8 +5,50 @@ Module containing various geometrical tasks
 import numpy as np
 from rasterio import features, transform
 from shapely.geometry import Polygon
+from skimage.morphology import binary_erosion, disk
 
 from eolearn.core import EOTask
+
+import logging
+LOGGER = logging.getLogger(__name__)
+
+
+class ErosionTask(EOTask):
+    """
+    The task performs an erosion to the provided mask
+
+    :param mask_feature: The mask which is to be eroded
+    :type mask_feature: (FeatureType, str)
+    :param disk_radius: Radius of the erosion disk (in pixels)
+    :type disk_radius: int
+    """
+
+    def __init__(self, mask_feature, disk_radius=1, erode_labels=None, no_data_label=0):
+        self.mask_type, self.mask_name = next(iter(self._parse_features(mask_feature)))
+        self.disk_radius = disk_radius
+        self.erode_labels = erode_labels
+        self.no_data_label = no_data_label
+
+
+    def execute(self, eopatch):
+
+        if self.disk_radius is None or self.disk_radius < 1 or not isinstance(self.disk_radius, int):
+            LOGGER.warning('Disk radius should be an integer larger than 0! Ignoring erosion task.')
+            return eopatch
+
+        labels = eopatch[self.mask_type][self.mask_name].squeeze()
+        if self.erode_labels is None:
+            self.erode_labels = np.unique(labels)
+
+        mask_values = np.zeros(labels.shape, dtype=np.bool)
+        for label in self.erode_labels:
+            label_mask = (labels == label)
+            label_mask = binary_erosion(label_mask, disk(self.disk_radius))
+            mask_values |= label_mask
+
+        labels[~mask_values] = self.no_data_label
+        eopatch[self.mask_type][self.mask_name] = np.expand_dims(labels, axis=-1)
+        return eopatch
 
 
 class VectorToRaster(EOTask):
