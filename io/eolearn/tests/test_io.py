ddc841ca2392a7a293286cab33d7d00f52b27a8f
AleksMat <matej.aleksandrov@sinergise.com>
2018-12-10 19:26:59 +0100

    fixed downloading SCL mask and added a unit test

--- io/eolearn/tests/test_io.py
+++ io/eolearn/tests/test_io.py
@@ -4,12 +4,11 @@ import logging
 import os
 import numpy
 
-from sentinelhub import BBox, CRS
-from sentinelhub import DataSource
+from sentinelhub import BBox, CRS, DataSource, ServiceType
 from sentinelhub.time_utils import iso_to_datetime
 
 from eolearn.io import *
-from eolearn.core import EOPatch
+from eolearn.core import EOPatch, FeatureType
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -22,12 +21,13 @@ class TestEOPatch(unittest.TestCase):
         """
 
         def __init__(self, name, request, bbox, time_interval,
-                     eop=None, layer=None, data_size=None, timestamp_length=None):
+                     eop=None, layer=None, data_size=None, timestamp_length=None, feature_type=FeatureType.DATA):
             self.name = name
             self.request = request
             self.layer = layer
             self.data_size = data_size
             self.timestamp_length = timestamp_length
+            self.feature_type = feature_type
 
             if eop is None:
                 self.eop = request.execute(bbox=bbox, time_interval=time_interval)
@@ -189,7 +189,7 @@ class TestEOPatch(unittest.TestCase):
                 bbox=bbox,
                 time_interval=time_interval,
                 eop=EOPatch()
-            ),
+            )
         ]
 
         cls.update_patches = [
@@ -224,6 +224,25 @@ class TestEOPatch(unittest.TestCase):
                 ),
                 bbox=bbox,
                 time_interval=time_interval,
+                feature_type=FeatureType.DATA_TIMELESS
+            ),
+            cls.TaskTestCase(
+                name='Sen2Cor_to_existing_patch',
+                layer='SCL',
+                data_size=1,
+                timestamp_length=55,
+                eop=cls.eeop.__deepcopy__(),
+                request=AddSen2CorClassificationFeature(
+                    sen2cor_classification='SCL',
+                    layer='BANDS-S2-L2A',
+                    service_type=ServiceType.WCS,
+                    size_x=resx,
+                    size_y=resy,
+                    instance_id=instance_id
+                ),
+                bbox=bbox,
+                time_interval=time_interval,
+                feature_type=FeatureType.MASK
             ),
         ]
 
@@ -249,7 +268,8 @@ class TestEOPatch(unittest.TestCase):
     def test_auto_feature_presence(self):
         for task in self.task_cases:
             with self.subTest(msg='Test case {}'.format(task.name)):
-                self.assertTrue(task.layer in task.eop.data or task.eop.data_timeless)
+                self.assertTrue(task.layer in task.eop[task.feature_type],
+                                msg='Feature {} should be in {}'.format(task.layer, task.feature_type))
                 self.assertTrue('IS_DATA' in task.eop.mask)
 
     def test_feature_dimension(self):
