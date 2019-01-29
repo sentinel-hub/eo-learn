5b989162cdb39ff06302e69cc1ab100c9a132bc3
William Ouellette <william.ouellette@gmail.com>
2019-01-29 18:41:21 +0100

    Updated ExportToTiff method to support band_count and time_count
    Multiple formats implemented as discussed, in case @mlubej did not work on it yet.

--- io/eolearn/io/local_io.py
+++ io/eolearn/io/local_io.py
@@ -18,10 +18,12 @@ class ExportToTiff(SaveToDisk):
     :type feature: (FeatureType, str)
     :param folder: root directory where all Geo-Tiff images will be saved
     :type folder: str
-    :param band_count: Number of bands to be added to tiff image
-    :type band_count: int
-    :param time_count: Number of dates to be added to tiff image
-    :type time_count: int
+    :param band_count: Bands to be added to tiff image. Bands are represented by an integer `band_n`, 
+    a tuple in the form `(start_band, end_band)` or a list in the form `[band_1, band_2,...,band_n]`.
+    :type band_count: int, tuple or list
+    :param time_count: Dates to be added to tiff image. Dates are represented by an integer `date_n`, 
+    a tuple in the form `(start_date, end_date)` or a list in the form `[date_1, date_2,...,date_n]`.
+    :type time_count: int, tuple or list
     :param image_dtype: Type of data to be saved into tiff image
     :type image_dtype: numpy.dtype
     :param no_data_value: Value of pixels of tiff image with no data in EOPatch
@@ -47,20 +49,40 @@ class ExportToTiff(SaveToDisk):
         filename_list = []
         
         if type(self.band_count) is list: 
+            if [date for date in self.band_count if type(elem) != int]:
+                raise ValueError('Invalid format in {} list, expected integers'.format(self.band_count))
             array_sub = array[...,np.array(self.band_count)-1]
         elif type(self.band_count) is tuple:
-            array_sub = array[...,np.nonzero(np.where(bands >= band_count[0] and bands <= band_count[1],bands,0))]
-        elif type(self.band_count) is int:
+            if tuple(map(type, self.band_count)) != (int, int):
+                raise ValueError('Invalid format in {} tuple, expected integers'.format(self.band_count))
+            array_sub = array[...,np.nonzero(np.where(bands >= self.band_count[0] and bands <= self.band_count[1],bands,0))]
+        elif type(self.band_count) == int:
             array_sub = array[...,self.band_count-1]
+        else:
+            raise ValueError('Invalid format in {}, expected int, tuple or list'.format(self.band_count))
+            
         band_dim = len(array_sub.shape[-1])
 
         if feature_type in [FeatureType.DATA, FeatureType.MASK]:
-            if type(self.time_count) is list: 
+            if type(self.time_count) == list:
+                if [date for date in self.time_count if type(elem) != int]:
+                    raise ValueError('Invalid format in {} list, expected integers'.format(self.time_count))
                 array_sub = array_sub[np.array(self.time_count)-1,...]
-            elif type(self.time_count) is tuple:
-                array_sub = array_sub[np.nonzero(np.where(dates >= time_count[0] and dates <= time_count[1],dates,0)),...]
-            elif type(self.time_count) is int:
+            elif type(self.time_count) == tuple:
+                if tuple(map(type, self.time_count)) == (str, str):
+                    start_date = parser.parse(self.time_count[0])
+                    end_date = parser.parse(self.time_count[1])
+                elif tuple(map(type, self.time_count)) == (datetime, datetime):
+                    start_date = self.time_count[0]
+                    end_date = self.time_count[1]
+                else:
+                    raise ValueError('Invalid format in {} tuple, expected datetimes or strings'.format(self.time_count))
+                array_sub = array_sub[np.nonzero(np.where(dates >= start_date and dates <= end_date, dates, 0)),...]
+            elif type(self.time_count) == int:
                 array_sub = array_sub[self.time_count-1,...]
+            else:
+                raise ValueError('Invalid format in {}, expected int, tuple or list'.format(self.time_count))
+                
             time_dim = len(array_sub.shape[0])
             width = array_sub.shape[2]
             height = array_sub.shape[1]
