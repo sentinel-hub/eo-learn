e13102dd4385ee164a9f72a17325e814408fa20d
Drew Bollinger <drew@developmentseed.org>
2019-03-26 17:21:50 -0400

    Lint

--- io/eolearn/io/tile_io.py
+++ io/eolearn/io/tile_io.py
@@ -2,12 +2,10 @@
 Module containing tasks used for reading from non-SentinelHub tile services
 """
 
-import os.path
 import math
 from io import BytesIO
 
 from mercantile import tiles, ul
-import rasterio
 import numpy as np
 import requests
 
@@ -16,13 +14,13 @@ from sentinelhub.constants import CRS
 from sentinelhub.io_utils import read_image
 
 TILE_SIZE = 256
+CIRC = 40075016.686 # circumference of the earth in meters
 
 def get_zoom(res, lat):
     """ For a given resolution and latitude, return the lowest integer zoom level
     which will give 256x256 tiles with better resolution than the input"""
     # calculation from https://wiki.openstreetmap.org/wiki/Zoom_levels#Distance_per_pixel_math
-    C = 40075016.686 # circumference of the earth in meters
-    return math.ceil((math.log(math.cos(lat / 180 * math.pi) * C / res) - math.log(TILE_SIZE)) / math.log(2))
+    return math.ceil((math.log(math.cos(lat / 180 * math.pi) * CIRC / res) - math.log(TILE_SIZE)) / math.log(2))
 
 def get_resolution(zoom, lat):
     """ For a given zoom level and latitude, return pixel size in degrees"""
@@ -32,6 +30,17 @@ def get_resolution(zoom, lat):
 def parse_numeric(string):
     """return an integer from a string containing one"""
     return int(''.join(x for x in string if x.isdigit()))
+
+def get_window(zoom, maximum_latitude, image_tile_xs, image_tile_ys, latlng_bounds):
+    """return the bbox as a window in pixel bounds of a larger image tile"""
+    image_tile_origin = ul(min(image_tile_xs), min(image_tile_ys), zoom)
+    x_res = get_resolution(zoom, maximum_latitude)
+    y_res = 180 / math.pow(2, zoom) / TILE_SIZE
+    top = int((image_tile_origin.lat - latlng_bounds.max_y) / y_res)
+    left = int((latlng_bounds.min_x - image_tile_origin.lng) / x_res)
+    bottom = int((image_tile_origin.lat - latlng_bounds.min_y) / y_res)
+    right = int((latlng_bounds.max_x - image_tile_origin.lng) / x_res)
+    return (top, bottom, left, right)
 
 class MapboxXYZInput(EOTask):
     """ Use a Mapbox raster tile service as input to a MASK_TIMELESS feature
@@ -44,8 +53,8 @@ class MapboxXYZInput(EOTask):
     :type access_token: str
     """
 
-    def __init__(self, id, mask_name, access_token):
-        self.id = id
+    def __init__(self, mapbox_id, mask_name, access_token):
+        self.mapbox_id = mapbox_id
         self.mask_name = mask_name
         self.access_token = access_token
 
@@ -71,11 +80,17 @@ class MapboxXYZInput(EOTask):
 
         # find all necessary tiles and download them into a single 3-band image
         covering_tiles = list(tiles(*latlng_bounds, zoom))
-        image_tile_xs = set([t.x for t in covering_tiles])
-        image_tile_ys = set([t.y for t in covering_tiles])
-        image_tile = np.empty(shape=(len(image_tile_ys) * TILE_SIZE, len(image_tile_xs) * TILE_SIZE, 3)).astype(np.uint8)
+        image_tile_xs = {t.x for t in covering_tiles}
+        image_tile_ys = {t.y for t in covering_tiles}
+        image_tile = np.empty(shape=(
+            len(image_tile_ys) * TILE_SIZE,
+            len(image_tile_xs) * TILE_SIZE,
+            3
+        )).astype(np.uint8)
         for tile in covering_tiles:
-            url = 'http://api.mapbox.com/v4/{}/{}/{}/{}.png?access_token={}'.format(self.id, tile.z, tile.x, tile.y, self.access_token)
+            url = 'http://api.mapbox.com/v4/{}/{}/{}/{}.png?access_token={}'.format(
+                self.mapbox_id, tile.z, tile.x, tile.y, self.access_token
+            )
             resp = requests.get(url)
             torg = ( # tile origin
                 (tile.x - min(image_tile_xs)) * TILE_SIZE,
@@ -84,12 +99,8 @@ class MapboxXYZInput(EOTask):
             image_tile[torg[1]:torg[1] + TILE_SIZE, torg[0]:torg[0] + TILE_SIZE] = read_image(BytesIO(resp.content))
 
         # read into the larger image_tile
-        image_tile_origin = ul(min(image_tile_xs), min(image_tile_ys), zoom)
-        res = get_resolution(zoom, maximum_latitude)
-        top = int((image_tile_origin.lat - latlng_bounds.max_y) / res)
-        left = int((latlng_bounds.min_x - image_tile_origin.lng) / res)
-        bottom = int((image_tile_origin.lat - latlng_bounds.min_y) / res)
-        right = int((latlng_bounds.max_x - image_tile_origin.lng) / res)
+        top, bottom, left, right = get_window(zoom, maximum_latitude, image_tile_xs, image_tile_ys, latlng_bounds)
+
 
         eopatch[FeatureType.MASK_TIMELESS][self.mask_name] = image_tile[top:bottom, left:right]
         return eopatch
