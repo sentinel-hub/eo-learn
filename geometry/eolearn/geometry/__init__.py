41bfd39a6bf48745698775f430a2109d0c71d651
AleksMat <matej.aleksandrov@sinergise.com>
2019-06-04 17:17:38 +0200

    added MarkSegmentationBoundaries task to init

--- geometry/eolearn/geometry/__init__.py
+++ geometry/eolearn/geometry/__init__.py
@@ -4,6 +4,6 @@ Subpackage containing EOTasks for geometrical transformations
 
 from .utilities import ErosionTask, VectorToRaster, RasterToVector
 from .sampling import PointSamplingTask, PointSampler, PointRasterSampler
-from .superpixel import SuperpixelSegmentation, FelzenszwalbSegmentation, SlicSegmentation
+from .superpixel import SuperpixelSegmentation, FelzenszwalbSegmentation, SlicSegmentation, MarkSegmentationBoundaries
 
 __version__ = '0.4.2'
