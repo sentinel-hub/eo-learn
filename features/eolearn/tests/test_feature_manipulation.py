4ad7bba060825c1047095885d420e296a5772e09
iovsn <jovan.visnjic@sinergise.com>
2019-07-26 12:13:38 +0200

    added ValueFilloutTask

--- features/eolearn/tests/test_feature_manipulation.py
+++ features/eolearn/tests/test_feature_manipulation.py
@@ -1,8 +1,8 @@
 import unittest
 import datetime
 
-from eolearn.features import FilterTimeSeries
-from eolearn.core import EOPatch
+from eolearn.features import FilterTimeSeries, ValueFilloutTask
+from eolearn.core import EOPatch, FeatureType
 
 import numpy as np
 
@@ -42,6 +42,52 @@ class TestFeatureManipulation(unittest.TestCase):
 
         self.assertEqual(new_interval, updated_interval)
         self.assertEqual(new_timestamps, updated_timestamps)
+
+    def test_value_fillout(self):
+        feature = (FeatureType.DATA, 'TEST')
+        shape = (8, 10, 10, 5)
+        data = np.random.randint(0, 100, size=shape).astype(np.float)
+        eopatch = EOPatch(data={'TEST': data})
+
+        self.assertRaises(ValueError, ValueFilloutTask, feature, operations='x')
+        self.assertRaises(ValueError, ValueFilloutTask, feature, operations=4)
+
+        self.assertRaises(ValueError, ValueFilloutTask.fill, None, operation='f')
+        self.assertRaises(ValueError, ValueFilloutTask.fill, np.zeros((4, 5)), operation='x')
+
+        # nothing to be filled, return the same eopatch object immediately
+        eopatch_new = ValueFilloutTask(feature, operations='fb', axis=0)(eopatch)
+        self.assertEqual(eopatch, eopatch_new)
+
+        eopatch[feature][0, 0, 0, :] = np.nan
+
+        # filling forward temporally should not fill nans
+        eopatch = ValueFilloutTask(feature, operations='f', axis=0)(eopatch)
+        self.assertTrue(np.isnan(eopatch[feature][0, 0, 0, :]).all())
+
+        # filling in any direction along axis=-1 should also not fill nans since all neighbors are nans
+        eopatch = ValueFilloutTask(feature, operations='fb', axis=-1)(eopatch)
+        self.assertTrue(np.isnan(eopatch[feature][0, 0, 0, :]).all())
+
+        # filling nans backwards temporally should fill nans
+        eopatch = ValueFilloutTask(feature, operations='b', axis=0)(eopatch)
+        self.assertFalse(np.isnan(eopatch[feature]).any())
+
+        # try filling something else than nan (e.g.: -1)
+        eopatch[feature][0, :, 0, 0] = -1
+        eopatch = ValueFilloutTask(feature, operations='b', value=-1, axis=-1)(eopatch)
+        # !!
+        self.assertFalse(np.isnan(eopatch[feature]).any())
+
+        # [nan, 1, nan, 2, ... ]  ---('fb')---> [1, 1, 1, 2, ... ]
+        eopatch[feature][0, 0, 0, 0:4] = [np.nan, 1, np.nan, 2]
+        eopatch = ValueFilloutTask(feature, operations='fb', axis=-1)(eopatch)
+        self.assertTrue(np.array_equal(eopatch[feature][0, 0, 0, 0:4], [1, 1, 1, 2]))
+
+        # [nan, 1, nan, 2, ... ]  ---('bf')---> [1, 1, 2, 2, ... ]
+        eopatch[feature][0, 0, 0, 0:4] = [np.nan, 1, np.nan, 2]
+        eopatch = ValueFilloutTask(feature, operations='bf', axis=-1)(eopatch)
+        self.assertTrue(np.array_equal(eopatch[feature][0, 0, 0, 0:4], [1, 1, 2, 2]))
 
 
 if __name__ == '__main__':
