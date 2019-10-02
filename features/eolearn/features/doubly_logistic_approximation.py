cc28a483cf95697bc4ab82030771da69c3cda5d0
Beno Šircelj <benosircelj@gmail.com>
2019-10-02 15:19:08 +0200

    Added multiprocessing and improved tests.

--- features/eolearn/features/doubly_logistic_approximation.py
+++ features/eolearn/features/doubly_logistic_approximation.py
@@ -1,51 +1,20 @@
 import numpy as np
-from eolearn.core import EOTask, FeatureType
+from eolearn.core import EOTask
 from scipy.optimize import curve_fit
+import multiprocessing
+import itertools as it
 
 
-def _optimizing_function(x, c1, c2, a1, a2, a3, a4, a5):
-    """Double logistic function which is needed for scipy.optimize
-    :param x, c1, c2, a1, a2, a3, a4, a5: All inputs are the parameters which are optimized
-    """
-
-    def left(y):
-        return np.exp(-((a1 - y) / a4) ** a5)
-
-    def right(y):
-        return np.exp(-((y - a1) / a2) ** a3)
-
-    return c1 + c2 * np.piecewise(x, [x < a1, x >= a1], [left, right])
-
-
-def _fit_optimize(x, y, p0=None):
-    """
-    :param x: Vertical coordinates of points
-    :type x: List of floats
-    :param y: Horizontal coordinates of points
-    :type y: List of floats
-    :param p0: Initial parameter guess
-    :type p0: List of floats
-    :return: List of optimized parameters [c1, c2, a1, a2, a3, a4, a5]
-    """
-    # Normalization
-    norm = (max(x) - min(x))
-    x = x / norm
-
-    bounds_lower = [-1, -np.inf, x[0], 0.15, 1, 0.15, 1, ]
-    bounds_upper = [1, np.inf, x[-1], np.inf, np.inf, np.inf, np.inf, ]
-    # bounds=(bounds_lower, bounds_upper),
-    if p0 is None:
-        p0 = [np.mean(x / norm), 0.2, 0.5, 0.15, 10, 0.15, 10]
-    optimal_values = curve_fit(_optimizing_function, x, y, bounds=(bounds_lower, bounds_upper), maxfev=1000000,
-                               absolute_sigma=True)
-    return optimal_values[0]
+def doubly_logistic(x, c1, c2, a1, a2, a3, a4, a5):
+    return c1 + c2 * np.piecewise(x, [x < a1, x >= a1],
+                                  [lambda y: np.exp(-((a1 - y) / a4) ** a5),
+                                   lambda y: np.exp(-((y - a1) / a2) ** a3)])
 
 
 class DoublyLogisticApproximationTask(EOTask):
     """
     EOTask class for calculation of doubly logistic approximation on each pixel for a feature. The task creates new
     feature with the function parameters for each pixel as vectors.
-
     :param feature: A feature on which the function will be approximated
     :type feature: str
     :param new_feature: Name of the new feature where parameters of the function are saved
@@ -54,22 +23,62 @@ class DoublyLogisticApproximationTask(EOTask):
     :type p0: List of floats length 7 corresponding to each parameter
     """
 
-    def __init__(self, feature, new_feature='DOUBLY_LOGISTIC_PARAM', p0=None):
+    def __init__(self, feature, new_feature='DOUBLY_LOGISTIC_PARAM', p0=None, mask_data=False):
         self.feature = feature
         self.p0 = p0
         self.new_feature = new_feature
+        self.mask_data = mask_data
+
+    def _pool_wrapper(self, parameters):
+        """
+        Wrapper for _fit_optimize function because Pool accepts only a one dimensional array but _fit_optimize needs two
+        :param parameters: Tuple with two lists of floats corresponding to horizontal and vertical axis
+        :return: List of optimized parameters [c1, c2, a1, a2, a3, a4, a5]
+        """
+        return self._fit_optimize(*parameters)
+
+    def _fit_optimize(self, x, y):
+        """
+        :param x: Horizontal coordinates of points
+        :type x: List of floats
+        :param y: Vertical coordinates of points
+        :type y: List of floats
+        :return: List of optimized parameters [c1, c2, a1, a2, a3, a4, a5]
+        """
+        bounds_lower = [np.min(y), -np.inf, x[0], 0.15, 1, 0.15, 1, ]
+        bounds_upper = [np.max(y), np.inf, x[-1], np.inf, np.inf, np.inf, np.inf, ]
+        if self.p0 is None:
+            self.p0 = [np.mean(y), 0.2, (x[-1] - x[0]) / 2, 0.15, 10, 0.15, 10]
+        optimal_values = curve_fit(doubly_logistic, x, y, self.p0,
+                                   bounds=(bounds_lower, bounds_upper), maxfev=1000000,
+                                   absolute_sigma=True)
+        return optimal_values[0]
 
     def execute(self, eopatch):
         data = eopatch.data[self.feature].squeeze(axis=3)
-        print(data)
-        t, width, height = data.shape
-        all_parameters = np.zeros((width, height, 7))
 
-        for i in range(width):
-            for j in range(height):
-                all_parameters[i, j, :] = _fit_optimize(data[:, i, j], range(t), self.p0)
+        times = np.asarray([time.toordinal() for time in eopatch.timestamp])
+        times = (times - times[0]) / (times[-1] - times[0])
 
-        all_parameters = np.expand_dims(all_parameters, axis=3)
-        eopatch.add_feature(FeatureType.DATA, self.new_feature, all_parameters)
+        t, h, w = data.shape
+
+        if self.mask_data:
+            valid_data_mask = eopatch.mask['VALID_DATA']
+        else:
+            valid_data_mask = np.ones((t, h, w), dtype=bool)
+
+        valid_parameters = []
+
+        # Make an array that can be passed to pool function
+        for ih, iw in it.product(range(h), range(w)):
+            valid_parameters.append((times[valid_data_mask[:, ih, iw].squeeze()],
+                                     data[:, ih, iw][valid_data_mask[:, ih, iw].squeeze()]))
+
+        pool = multiprocessing.Pool(processes=multiprocessing.cpu_count())
+        all_parameters = pool.map(self._pool_wrapper, valid_parameters)
+
+        all_parameters = np.reshape(all_parameters, (h, w, 7))
+
+        eopatch.data_timeless[self.new_feature] = all_parameters
 
         return eopatch
--- features/eolearn/tests/test_doubly_logistic_approximation.py
+++ features/eolearn/tests/test_doubly_logistic_approximation.py
@@ -1,45 +1,73 @@
 import unittest
+
 import numpy as np
-from eolearn.core import EOPatch, FeatureType
-from features.eolearn.features import DoublyLogisticApproximationTask
+from eolearn.core import EOPatch
+from features.eolearn.features.doubly_logistic_approximation import \
+    DoublyLogisticApproximationTask
+from datetime import datetime
 
 
 class TestDoublyLogisticApproximation(unittest.TestCase):
-    patch = EOPatch()
 
-    @staticmethod
+    @classmethod
     def setUpClass(cls):
-        data = np.array([0, 0.05, 0.2, 0.5, 0.81, 1, 0.95, 0.51, 0.03, 0.001, 0.001])
-        for i in range(3):
-            data = np.expand_dims(data, axis=1)
+        dates = [datetime(2017, 1, 1), datetime(2017, 1, 11), datetime(2017, 2, 7), datetime(2017, 2, 17),
+                 datetime(2017, 2, 20), datetime(2017, 3, 9), datetime(2017, 3, 12), datetime(2017, 3, 22),
+                 datetime(2017, 3, 29), datetime(2017, 4, 1), datetime(2017, 4, 11), datetime(2017, 4, 21),
+                 datetime(2017, 5, 1), datetime(2017, 5, 8), datetime(2017, 5, 11), datetime(2017, 5, 18),
+                 datetime(2017, 5, 21), datetime(2017, 5, 28), datetime(2017, 5, 31), datetime(2017, 6, 10),
+                 datetime(2017, 6, 17), datetime(2017, 6, 20), datetime(2017, 6, 27), datetime(2017, 6, 30),
+                 datetime(2017, 7, 2), datetime(2017, 7, 7), datetime(2017, 7, 10), datetime(2017, 7, 12),
+                 datetime(2017, 7, 15), datetime(2017, 7, 17), datetime(2017, 7, 20), datetime(2017, 7, 22),
+                 datetime(2017, 7, 25), datetime(2017, 7, 30), datetime(2017, 8, 1), datetime(2017, 8, 4),
+                 datetime(2017, 8, 6), datetime(2017, 8, 9), datetime(2017, 8, 11), datetime(2017, 8, 21),
+                 datetime(2017, 8, 24), datetime(2017, 8, 26), datetime(2017, 8, 29), datetime(2017, 8, 31),
+                 datetime(2017, 9, 5), datetime(2017, 9, 18), datetime(2017, 9, 23), datetime(2017, 9, 28),
+                 datetime(2017, 9, 30), datetime(2017, 10, 5), datetime(2017, 10, 8), datetime(2017, 10, 10),
+                 datetime(2017, 10, 13), datetime(2017, 10, 15), datetime(2017, 10, 18), datetime(2017, 10, 20),
+                 datetime(2017, 10, 25), datetime(2017, 10, 30), datetime(2017, 11, 4), datetime(2017, 11, 19),
+                 datetime(2017, 11, 24), datetime(2017, 11, 27), datetime(2017, 12, 2), datetime(2017, 12, 7),
+                 datetime(2017, 12, 17), datetime(2017, 12, 19), datetime(2017, 12, 22), datetime(2017, 12, 24),
+                 datetime(2017, 12, 29)]
 
-        cls.eopatch.add_feature(FeatureType.DATA, 'TEST', data)
-        cls.eopatch = DoublyLogisticApproximationTask('TEST', 'TEST_OUT')(cls.eopatch)
+        values = [0.059783712, 0.06304545, np.NaN, 0.07635699, np.NaN, 0.07554149, np.NaN, np.NaN, 0.095581025,
+                  0.09109493, np.NaN, 0.114999376, np.NaN, np.NaN, np.NaN, 0.12209105, 0.089099824, 0.12314385,
+                  np.NaN, np.NaN, np.NaN, 0.13705324, np.NaN, np.NaN, np.NaN, 0.26388252, 0.19771394, 0.27082473,
+                  np.NaN, 0.3250511, 0.24847558, 0.35576594, 0.3713841, 0.30338106, 0.4179747, 0.326186, np.NaN,
+                  np.NaN, np.NaN, 0.69031024, 0.58324015, 0.68232125, 0.59130245, 0.655362, 0.7490687, np.NaN,
+                  0.45670646, 0.39466715, 0.43877497, 0.40336365, 0.2609013, np.NaN, 0.13757367, 0.12950855,
+                  0.13163383, 0.13427204, np.NaN, 0.117703386, 0.12337628, 0.013523403, 0.076479584, 0.058155004,
+                  np.NaN, 0.04863815, 0.060789138, 0.063743524, np.NaN, 0.044066854, 0.11930587]
+
+        cls.eopatch = EOPatch(timestamp=list(dates))
+        cls.eopatch.timestamp = list(dates)
+        cls.eopatch.data["TEST"] = np.array(values).reshape(-1, 1, 1, 1)
+        cls.eopatch.mask["VALID_DATA"] = ~np.isnan(cls.eopatch.data["TEST"])
+        cls.eopatch = DoublyLogisticApproximationTask('TEST', 'TEST_OUT', mask_data=True).execute(cls.eopatch)
 
     def test_parameters(self):
-        c1, c2, a1, a2, a3, a4, a5 = self.eopatch.data['TEST_OUT'].squeeze()
+        c1, c2, a1, a2, a3, a4, a5 = self.eopatch.data_timeless['TEST_OUT'].squeeze()
         delta = 0.1
-        print(c1)
 
-        exp_c1 = 1
+        exp_c1 = 0.03
         self.assertAlmostEqual(c1, exp_c1, delta=delta)
 
-        exp_c2 = 8.50
+        exp_c2 = 0.56
         self.assertAlmostEqual(c2, exp_c2, delta=delta)
 
-        exp_a1 = 0.88
+        exp_a1 = 0.64
         self.assertAlmostEqual(a1, exp_a1, delta=delta)
 
-        exp_a2 = 5.51
+        exp_a2 = 0.15
         self.assertAlmostEqual(a2, exp_a2, delta=delta)
 
-        exp_a3 = 16.43
+        exp_a3 = 3.07
         self.assertAlmostEqual(a3, exp_a3, delta=delta)
 
-        exp_a4 = 0.46
+        exp_a4 = 0.15
         self.assertAlmostEqual(a4, exp_a4, delta=delta)
 
-        exp_a5 = 7.02
+        exp_a5 = 1
         self.assertAlmostEqual(a5, exp_a5, delta=delta)
 
 
