3c0483bd26e0bb7654472c514c0b2106906ca050
Beno Šircelj <benosircelj@gmail.com>
2019-10-15 15:10:05 +0200

    Removed multiprocessing.

--- features/eolearn/features/doubly_logistic_approximation.py
+++ features/eolearn/features/doubly_logistic_approximation.py
@@ -1,14 +1,17 @@
-import numpy as np
-from eolearn.core import EOTask
-from scipy.optimize import curve_fit
-import multiprocessing
+"""
+Module for calculating doubly logistic approximation.
+"""
 import itertools as it
+import numpy as np
+from scipy.optimize import curve_fit
+from eolearn.core import EOTask
 
 
-def doubly_logistic(x, c1, c2, a1, a2, a3, a4, a5):
-    return c1 + c2 * np.piecewise(x, [x < a1, x >= a1],
-                                  [lambda y: np.exp(-((a1 - y) / a4) ** a5),
-                                   lambda y: np.exp(-((y - a1) / a2) ** a3)])
+def doubly_logistic(middle, initial_value, scale, a1, a2, a3, a4, a5):
+    # pylint: invalid-name
+    return initial_value + scale * np.piecewise(middle, [middle < a1, middle >= a1],
+                                                [lambda y: np.exp(-((a1 - y) / a4) ** a5),
+                                                 lambda y: np.exp(-((y - a1) / a2) ** a3)])
 
 
 class DoublyLogisticApproximationTask(EOTask):
@@ -19,65 +22,57 @@ class DoublyLogisticApproximationTask(EOTask):
     :type feature: str
     :param new_feature: Name of the new feature where parameters of the function are saved
     :type new_feature: str
-    :param p0: Initial parameter guess
-    :type p0: List of floats length 7 corresponding to each parameter
+    :param initial_parameters: Initial parameter guess
+    :type initial_parameters: List of floats length 7 corresponding to each parameter
     """
 
-    def __init__(self, feature, new_feature='DOUBLY_LOGISTIC_PARAM', p0=None, mask_data=False):
+    def __init__(self, feature, new_feature='DOUBLY_LOGISTIC_PARAM', initial_parameters=None, mask_data=False):
         self.feature = feature
-        self.p0 = p0
+        self.initial_parameters = initial_parameters
         self.new_feature = new_feature
         self.mask_data = mask_data
 
-    def _pool_wrapper(self, parameters):
+    def _fit_optimize(self, x_axis, y_axis):
         """
-        Wrapper for _fit_optimize function because Pool accepts only a one dimensional array but _fit_optimize needs two
-        :param parameters: Tuple with two lists of floats corresponding to horizontal and vertical axis
+        :param x_axis: Horizontal coordinates of points
+        :type x_axis: List of floats
+        :param y_axis: Vertical coordinates of points
+        :type y_axis: List of floats
         :return: List of optimized parameters [c1, c2, a1, a2, a3, a4, a5]
         """
-        return self._fit_optimize(*parameters)
-
-    def _fit_optimize(self, x, y):
-        """
-        :param x: Horizontal coordinates of points
-        :type x: List of floats
-        :param y: Vertical coordinates of points
-        :type y: List of floats
-        :return: List of optimized parameters [c1, c2, a1, a2, a3, a4, a5]
-        """
-        bounds_lower = [np.min(y), -np.inf, x[0], 0.15, 1, 0.15, 1, ]
-        bounds_upper = [np.max(y), np.inf, x[-1], np.inf, np.inf, np.inf, np.inf, ]
-        if self.p0 is None:
-            self.p0 = [np.mean(y), 0.2, (x[-1] - x[0]) / 2, 0.15, 10, 0.15, 10]
-        optimal_values = curve_fit(doubly_logistic, x, y, self.p0,
+        bounds_lower = [np.min(y_axis), -np.inf, x_axis[0], 0.15, 1, 0.15, 1, ]
+        bounds_upper = [np.max(y_axis), np.inf, x_axis[-1], np.inf, np.inf, np.inf, np.inf, ]
+        if self.initial_parameters is None:
+            self.initial_parameters = [np.mean(y_axis), 0.2, (x_axis[-1] - x_axis[0]) / 2, 0.15, 10, 0.15, 10]
+        optimal_values = curve_fit(doubly_logistic, x_axis, y_axis, self.initial_parameters,
                                    bounds=(bounds_lower, bounds_upper), maxfev=1000000,
                                    absolute_sigma=True)
         return optimal_values[0]
 
     def execute(self, eopatch):
+        """
+        :param eopatch: Input eopatch with data on which the doubly logistic approximation is computed
+        :return: Output patch with doubly logistic approximation parameters
+        """
         data = eopatch.data[self.feature].squeeze(axis=3)
 
         times = np.asarray([time.toordinal() for time in eopatch.timestamp])
         times = (times - times[0]) / (times[-1] - times[0])
 
-        t, h, w = data.shape
+        time, height, width = data.shape
 
         if self.mask_data:
             valid_data_mask = eopatch.mask['VALID_DATA']
         else:
-            valid_data_mask = np.ones((t, h, w), dtype=bool)
+            valid_data_mask = np.ones((time, height, width), dtype=bool)
 
-        valid_parameters = []
+        all_parameters = np.zeros((height, width, 7))
 
-        # Make an array that can be passed to pool function
-        for ih, iw in it.product(range(h), range(w)):
-            valid_parameters.append((times[valid_data_mask[:, ih, iw].squeeze()],
-                                     data[:, ih, iw][valid_data_mask[:, ih, iw].squeeze()]))
+        for height_ind, width_ind in it.product(range(height), range(width)):
+            valid_curve = data[:, height_ind, width_ind][valid_data_mask[:, height_ind, width_ind].squeeze()]
+            valid_times = times[valid_data_mask[:, height_ind, width_ind].squeeze()]
 
-        pool = multiprocessing.Pool(processes=multiprocessing.cpu_count())
-        all_parameters = pool.map(self._pool_wrapper, valid_parameters)
-
-        all_parameters = np.reshape(all_parameters, (h, w, 7))
+            all_parameters[height_ind, width_ind] = self._fit_optimize(valid_times, valid_curve)
 
         eopatch.data_timeless[self.new_feature] = all_parameters
 
