9574ca11461a7674e724d78b507852022fb2c59f
iovsn <jovan.visnjic@sinergise.com>
2019-11-06 11:19:30 +0100

    implemented additional data support for Processing API (SCL, SNW...)

--- io/eolearn/io/processing_api.py
+++ io/eolearn/io/processing_api.py
@@ -10,44 +10,40 @@ from sentinelhub import WebFeatureService, MimeType, SentinelHubDownloadClient, 
      SHConfig
 import sentinelhub.sentinelhub_request as shr
 
-from eolearn.core import EOPatch, EOTask, FeatureType
+from eolearn.core import EOPatch, EOTask
 
 LOGGER = logging.getLogger(__name__)
-
-EVALSCRIPT = """
-    function setup() {{
-        return {{
-            input: [{{
-                bands: {bands},
-                units: "DN"
-            }}],
-            output: {{
-                id:"default",
-                bands: {num_bands},
-                sampleType: SampleType.UINT16
-            }}
-        }}
-    }}
-
-    function updateOutputMetadata(scenes, inputMetadata, outputMetadata) {{
-        outputMetadata.userData = {{ "norm_factor":  inputMetadata.normalizationFactor }}
-    }}
-
-    function evaluatePixel(sample) {{
-        return {samples}
-    }}
-"""
 
 
 class SentinelHubProcessingInput(EOTask):
     ''' A processing API input task that loads 16bit integer data and converts it to a 32bit float feature.
     '''
-    def __init__(self, feature_name, size_x, size_y, bbox, time_range, data_source, maxcc=1.0, time_difference=-1,
-                 bands=None, data_folder=None, max_threads=5):
+    def __init__(self, size_x, size_y, bbox, time_range, data_source, bands_feature=None, bands=None,
+                 additional_data=None, maxcc=1.0, time_difference=-1, cache_folder=None, max_threads=5):
         """
-        :param feature_name: Target feature into which to save the downloaded images.
+        :param size_x: Number of pixels in x dimension.
+        :type size_x: int
+        :param size_y: Number of pixels in y dimension.
+        :type size_y: int
+        :param bbox: Bounding box.
+        :type bbox: sentinelhhub.BBox
+        :param time_range: A range tuple of (date_from, date_to), defining a time range from which to acquire the data.
+        :type time_range: tuple(str, str)
+        :param bands_feature: Target feature into which to save the downloaded images.
+        :type bands_feature: tuple(sentinelhub.FeatureType, str)
+        :param bands: An array of band names.
+        :type bands: list[str]
+        :param additional_data: A list of additional data to be downloaded, such as SCL, SNW, dataMask, etc.
+        :type additional_data: list[tuple(sentinelhub.FeatureType, str)]
+        :param maxcc: Maximum cloud coverage.
+        :type maxcc: float
+        :param time_difference: Minimum allowed time difference in seconds, used when filtering dates.
+        :type time_difference: int
+        :param cache_folder: Path to cache_folder. If set to None (default) requests will not be cached.
+        :type cache_folder: str
+        :param max_threads: Maximum threads to be used when downloading data.
+        :type max_threads: int
         """
-        self.feature_name = feature_name
         self.size_x = size_x
         self.size_y = size_y
         self.bbox = bbox
@@ -55,13 +51,21 @@ class SentinelHubProcessingInput(EOTask):
         self.data_source = data_source
         self.maxcc = maxcc
         self.time_difference = dt.timedelta(seconds=time_difference)
-        self.data_folder = data_folder
-        self.bands = data_source.bands() if bands is None else bands
+        self.cache_folder = cache_folder
         self.max_threads = max_threads
+
+        self.bands_feature = bands_feature
+        self.bands = bands or data_source.bands() if bands_feature else []
+        self.additional_data = additional_data or []
+
+        self.all_bands = self.bands + [f_name for _, f_name in self.additional_data]
 
     @staticmethod
     def request_from_date(request, date):
         ''' Make a deep copy of a request and sets it's (from, to) range according to the provided 'date' argument
+
+        :param request: Path to cache_folder. If set to None (default) requests will not be cached.
+        :type request: str
         '''
         date_from, date_to = date, date + dt.timedelta(seconds=1)
         time_from, time_to = date_from.isoformat() + 'Z', date_to.isoformat() + 'Z'
@@ -71,15 +75,40 @@ class SentinelHubProcessingInput(EOTask):
             time_range = data['dataFilter']['timeRange']
             time_range['from'] = time_from
             time_range['to'] = time_to
+
         return request
 
     def generate_evalscript(self):
         ''' Generate the evalscript to be passed with the request, based on chosen bands
         '''
-        samples = ', '.join(['sample.{}'.format(band) for band in self.bands])
+        evalscript = """
+            function setup() {{
+                return {{
+                    input: [{{
+                        bands: {bands},
+                        units: "DN"
+                    }}],
+                    output: {{
+                        id:"default",
+                        bands: {num_bands},
+                        sampleType: SampleType.UINT16
+                    }}
+                }}
+            }}
+
+            function updateOutputMetadata(scenes, inputMetadata, outputMetadata) {{
+                outputMetadata.userData = {{ "norm_factor":  inputMetadata.normalizationFactor }}
+            }}
+
+            function evaluatePixel(sample) {{
+                return {samples}
+            }}
+        """
+
+        samples = ', '.join(['sample.{}'.format(band) for band in self.all_bands])
         samples = '[{}]'.format(samples)
-        script = EVALSCRIPT.format(bands=json.dumps(self.bands), num_bands=len(self.bands), samples=samples)
-        return script
+
+        return evalscript.format(bands=json.dumps(self.all_bands), num_bands=len(self.all_bands), samples=samples)
 
     def get_dates(self):
         ''' Make a WebFeatureService request to get dates and clean them according to self.time_difference
@@ -120,28 +149,33 @@ class SentinelHubProcessingInput(EOTask):
         url = SHConfig().get_sh_processing_api_url()
         headers = {"accept": "application/tar", 'content-type': 'application/json'}
         payloads = [self.request_from_date(request, date) for date in dates]
-        args = dict(url=url, headers=headers, data_folder=self.data_folder, hash_save=True, request_type='POST',
-                    data_type=MimeType.TAR)
+        args = dict(url=url, headers=headers, data_folder=self.cache_folder, hash_save=bool(self.cache_folder),
+                    request_type='POST', data_type=MimeType.TAR)
         request_list = [DownloadRequest(post_values=payload, **args) for payload in payloads]
 
-        LOGGER.debug('Starting %d processing requests.', len(request_list))
+        LOGGER.debug('Downloading %d requests of type %s', len(request_list), str(self.data_source))
+        LOGGER.debug('Downloading bands: [%s]', ', '.join(self.all_bands))
         client = SentinelHubDownloadClient()
         images = client.download_data(request_list)
         LOGGER.debug('Downloads complete')
 
         images = ((img['default.tif'], img['userdata.json']) for img in images)
-        images = [(img.astype(np.float32), meta.get('norm_factor', 0) if meta else 0) for img, meta in images]
+        images = [(img, meta.get('norm_factor', 0) if meta else 0) for img, meta in images]
 
         eopatch = EOPatch() if eopatch is None else eopatch
 
         shape = len(dates), self.size_y, self.size_x
 
-        if 'dataMask' in self.bands:
-            is_data_arrays = [img[..., -1:] for img, norm_factor in images]
-            eopatch[(FeatureType.MASK, 'IS_DATA')] = np.asarray(is_data_arrays).reshape(*shape, 1).astype(np.bool)
+        # exctract additional_data from the received images each as a separate feature
+        for f_type, f_name in self.additional_data:
+            idx = self.all_bands.index(f_name)
+            feature_arrays = [np.atleast_3d(img)[..., idx] for img, norm_factor in images]
+            eopatch[(f_type, f_name)] = np.asarray(feature_arrays).reshape(*shape, 1)
 
-        img_bands = len(self.bands) - 1 if 'dataMask' in self.bands else len(self.bands)
-        img_arrays = [img[..., slice(img_bands)] * norm_factor for img, norm_factor in images]
-        eopatch[(FeatureType.DATA, self.feature_name)] = np.asarray(img_arrays).reshape(*shape, img_bands)
+        # exctract bands from the received and save them as self.bands_feature
+        if self.bands:
+            img_bands = len(self.bands)
+            img_arrays = [img[..., slice(img_bands)].astype(np.float32) * norm_factor for img, norm_factor in images]
+            eopatch[self.bands_feature] = np.asarray(img_arrays).reshape(*shape, img_bands)
 
         return eopatch
---
+++ io/eolearn/tests/test_processing_io.py
@@ -0,0 +1,117 @@
+""" Testing SentinelHubProcessingInput
+"""
+
+import unittest
+from sentinelhub import CRS, BBox, DataSource
+
+from eolearn.io import SentinelHubProcessingInput
+from eolearn.core import FeatureType
+
+# import sys
+# import logging
+# logging.basicConfig(stream=sys.stdout)
+# logging.getLogger("eolearn.io.processing_api").setLevel(logging.DEBUG)
+# logging.getLogger("sentinelhub.sentinelhub_client").setLevel(logging.DEBUG)
+# logging.getLogger("sentinelhub.sentinelhub_rate_limit").setLevel(logging.DEBUG)
+
+class TestProcessingIO(unittest.TestCase):
+    """ Test cases for SentinelHubProcessingInput
+    """
+    size_x = 100
+    size_y = 100
+    bbox = BBox(bbox=[268892, 4624365, 268892+size_x*10, 4624365+size_y*10], crs=CRS.UTM_33N)
+    time_interval = ('2017-12-15', '2017-12-30')
+    maxcc = 0.8
+    time_difference = 60
+    max_threads = 3
+
+    def test_S2L1C(self):
+        """ Download S2L1C bands and dataMask
+        """
+        task = SentinelHubProcessingInput(
+            bands_feature=(FeatureType.DATA, 'BANDS'),
+            additional_data=[(FeatureType.MASK, 'dataMask')],
+            size_x=self.size_x,
+            size_y=self.size_y,
+            time_range=self.time_interval,
+            bbox=self.bbox,
+            maxcc=self.maxcc,
+            time_difference=self.time_difference,
+            data_source=DataSource.SENTINEL2_L1C,
+            max_threads=self.max_threads,
+        )
+
+        eopatch = task.execute()
+        bands = eopatch[(FeatureType.DATA, 'BANDS')]
+        is_data = eopatch[(FeatureType.MASK, 'dataMask')]
+
+        self.assertTrue(bands.shape == (4, 100, 100, 13))
+        self.assertTrue(is_data.shape == (4, 100, 100, 1))
+
+    def test_specific_bands(self):
+        """ Download S2L1C bands and dataMask
+        """
+        task = SentinelHubProcessingInput(
+            bands_feature=(FeatureType.DATA, 'BANDS'),
+            bands=["B01", "B02", "B03"],
+            size_x=self.size_x,
+            size_y=self.size_y,
+            time_range=self.time_interval,
+            bbox=self.bbox,
+            maxcc=self.maxcc,
+            time_difference=self.time_difference,
+            data_source=DataSource.SENTINEL2_L1C,
+            max_threads=self.max_threads
+        )
+
+        eopatch = task.execute()
+        bands = eopatch[(FeatureType.DATA, 'BANDS')]
+        self.assertTrue(bands.shape == (4, 100, 100, 3))
+
+    def test_S2L2A(self):
+        """ Download just SCL, without other bands
+        """
+        task = SentinelHubProcessingInput(
+            bands_feature=(FeatureType.DATA, 'BANDS'),
+            additional_data=[(FeatureType.MASK, 'dataMask')],
+            size_x=self.size_x,
+            size_y=self.size_y,
+            time_range=self.time_interval,
+            bbox=self.bbox,
+            maxcc=self.maxcc,
+            time_difference=self.time_difference,
+            data_source=DataSource.SENTINEL2_L2A,
+            max_threads=self.max_threads
+        )
+
+        eopatch = task.execute()
+        bands = eopatch[(FeatureType.DATA, 'BANDS')]
+        is_data = eopatch[(FeatureType.MASK, 'dataMask')]
+
+        self.assertTrue(bands.shape == (4, 100, 100, 12))
+        self.assertTrue(is_data.shape == (4, 100, 100, 1))
+
+    def test_scl_only(self):
+        """ Download just SCL, without any other bands
+        """
+        task = SentinelHubProcessingInput(
+            bands_feature=None,
+            additional_data=[(FeatureType.DATA, 'SCL')],
+            size_x=self.size_x,
+            size_y=self.size_y,
+            time_range=self.time_interval,
+            bbox=self.bbox,
+            maxcc=self.maxcc,
+            time_difference=self.time_difference,
+            data_source=DataSource.SENTINEL2_L2A,
+            max_threads=self.max_threads
+        )
+
+        eopatch = task.execute()
+        scl = eopatch[(FeatureType.DATA, 'SCL')]
+
+        self.assertTrue(scl.shape == (4, 100, 100, 1))
+
+
+if __name__ == "__main__":
+    unittest.main()
