98b0e7df6c947e1411657dfd531112d89161e3e8
AleksMat <matej.aleksandrov@sinergise.com>
2020-01-10 16:18:51 +0100

    versions to 0.7.0

--- io/eolearn/io/__init__.py
+++ io/eolearn/io/__init__.py
@@ -9,4 +9,4 @@ from .geopedia import AddGeopediaFeature
 from .local_io import ExportToTiff, ImportFromTiff
 from .processing_api import SentinelHubInputTask, SentinelHubDemTask
 
-__version__ = '0.6.0'
+__version__ = '0.7.0'
