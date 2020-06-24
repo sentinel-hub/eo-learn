6948a750f4ae14afd7921d87642786e7341a830c
Matej Batič <matej.batic@sinergise.com>
2020-06-24 14:38:46 +0200

    Rewritten SentinelHubIO notebook to use Processing API.

--- io/eolearn/io/__init__.py
+++ io/eolearn/io/__init__.py
@@ -7,6 +7,6 @@ from .sentinelhub_service import SentinelHubOGCInput, SentinelHubWMSInput, Senti
     DEMWMSInput, DEMWCSInput, AddSen2CorClassificationFeature
 from .geopedia import AddGeopediaFeature
 from .local_io import ExportToTiff, ImportFromTiff
-from .processing_api import SentinelHubInputTask, SentinelHubDemTask
+from .processing_api import SentinelHubInputTask, SentinelHubDemTask, SentinelHubInputBase, get_available_timestamps
 
 __version__ = '0.7.4'
