84ad0afb4db519edb6edfb0ef3e3aa1b18db24f8
AleksMat <matej.aleksandrov@sinergise.com>
2020-10-07 10:15:56 +0200

    moved merge_eopatch to a separate module and minor improvements

--- core/eolearn/core/eodata_merge.py
+++ core/eolearn/core/eodata_merge.py
@@ -9,7 +9,7 @@ Copyright (c) 2018-2020 William Ouellette
 This source code is licensed under the MIT license found in the LICENSE
 file in the root directory of this source tree.
 """
-
+import numpy as np
 import pandas as pd
 
 from .constants import FeatureType
