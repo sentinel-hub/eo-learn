e891feec0728a9e7305bacce710a5ad91ab95397
AleksMat <matej.aleksandrov@sinergise.com>
2020-10-19 17:21:09 +0200

    updated version to 0.8.0 and dependecies

--- coregistration/eolearn/coregistration/__init__.py
+++ coregistration/eolearn/coregistration/__init__.py
@@ -5,4 +5,4 @@ A collection of tools and EOTasks for image co-registration
 from .coregistration import RegistrationTask, InterpolationType, ECCRegistration, PointBasedRegistration, \
     ThunderRegistration
 
-__version__ = '0.7.3'
+__version__ = '0.8.0'
