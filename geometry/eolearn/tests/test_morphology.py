a7ed7a8df58892d82045f8be2215422a78198d7b
Ziga Luksic <ziga.luksic@live.com>
2021-08-10 18:01:33 +0200

    Update geometry an coregistration tests

--- geometry/eolearn/tests/test_morphology.py
+++ geometry/eolearn/tests/test_morphology.py
@@ -7,75 +7,59 @@ Copyright (c) 2017-2019 Blaž Sovdat, Nejc Vesel, Jovan Višnjić, Anže Zupanc,
 This source code is licensed under the MIT license found in the LICENSE
 file in the root directory of this source tree.
 """
-
-import unittest
-import os
-
+import pytest
 import numpy as np
 
-from eolearn.core import EOPatch, FeatureType
+from eolearn.core import FeatureType
 from eolearn.geometry import ErosionTask
 
 
-class TestErosion(unittest.TestCase):
-
-    TEST_PATCH_FILENAME = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../../../example_data',
-                                       'TestEOPatch')
-
-    @classmethod
-    def setUpClass(cls):
-        cls.classes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-    def test_erosion_value_error(self):
-        self.assertRaises(ValueError, ErosionTask, (FeatureType.MASK_TIMELESS, 'LULC', 'TEST'), disk_radius=None)
-        self.assertRaises(ValueError, ErosionTask, (FeatureType.MASK_TIMELESS, 'LULC', 'TEST'), disk_radius=0)
-        self.assertRaises(ValueError, ErosionTask, (FeatureType.MASK_TIMELESS, 'LULC', 'TEST'), disk_radius='a')
-
-    def test_erosion_full(self):
-        eopatch = EOPatch.load(self.TEST_PATCH_FILENAME, lazy_loading=True)
-        mask_before = eopatch.mask_timeless['LULC'].copy()
-
-        erosion_task = ErosionTask((FeatureType.MASK_TIMELESS, 'LULC', 'LULC_ERODED'), 1)
-        eopatch = erosion_task.execute(eopatch)
-
-        mask_after = eopatch.mask_timeless['LULC_ERODED'].copy()
-
-        self.assertFalse(np.all(mask_before == mask_after))
-
-        for label in self.classes:
-            if label == 0:
-                self.assertGreaterEqual(np.sum(mask_after == label), np.sum(mask_before == label),
-                                        msg="error in the erosion process")
-            else:
-                self.assertLessEqual(np.sum(mask_after == label), np.sum(mask_before == label),
-                                     msg="error in the erosion process")
-
-    def test_erosion_partial(self):
-        eopatch = EOPatch.load(self.TEST_PATCH_FILENAME, lazy_loading=True)
-        mask_before = eopatch.mask_timeless['LULC'].copy()
-
-        # skip forest and artificial surface
-        specific_labels = [0, 1, 3, 4]
-        erosion_task = ErosionTask(mask_feature=(FeatureType.MASK_TIMELESS, 'LULC', 'LULC_ERODED'),
-                                   disk_radius=1,
-                                   erode_labels=specific_labels)
-        eopatch = erosion_task.execute(eopatch)
-
-        mask_after = eopatch.mask_timeless['LULC_ERODED'].copy()
-
-        self.assertFalse(np.all(mask_before == mask_after))
-
-        for label in self.classes:
-            if label == 0:
-                self.assertGreaterEqual(np.sum(mask_after == label), np.sum(mask_before == label),
-                                        msg="error in the erosion process")
-            elif label in specific_labels:
-                self.assertLessEqual(np.sum(mask_after == label), np.sum(mask_before == label),
-                                     msg="error in the erosion process")
-            else:
-                self.assertTrue(np.array_equal(mask_after == label, mask_before == label),
-                                msg="error in the erosion process")
+CLASSES = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 
 
-if __name__ == '__main__':
-    unittest.main()
+@pytest.mark.parametrize('invalid_input', [None, 0, 'a'])
+def test_erosion_value_error(invalid_input):
+    with pytest.raises(ValueError):
+        ErosionTask((FeatureType.MASK_TIMELESS, 'LULC', 'TEST'), disk_radius=invalid_input)
+
+
+def test_erosion_full(test_eopatch):
+    mask_before = test_eopatch.mask_timeless['LULC'].copy()
+
+    erosion_task = ErosionTask((FeatureType.MASK_TIMELESS, 'LULC', 'LULC_ERODED'), 1)
+    eopatch = erosion_task.execute(test_eopatch)
+
+    mask_after = eopatch.mask_timeless['LULC_ERODED'].copy()
+
+    assert not np.all(mask_before == mask_after)
+
+    for label in CLASSES:
+        if label == 0:
+            assert np.sum(mask_after == label) >= np.sum(mask_before == label), 'Error in the erosion process'
+        else:
+            assert np.sum(mask_after == label) <= np.sum(mask_before == label), 'Error in the erosion process'
+
+
+def test_erosion_partial(test_eopatch):
+    mask_before = test_eopatch.mask_timeless['LULC'].copy()
+
+    # skip forest and artificial surface
+    specific_labels = [0, 1, 3, 4]
+    erosion_task = ErosionTask(
+        mask_feature=(FeatureType.MASK_TIMELESS, 'LULC', 'LULC_ERODED'),
+        disk_radius=1,
+        erode_labels=specific_labels
+    )
+    eopatch = erosion_task.execute(test_eopatch)
+
+    mask_after = eopatch.mask_timeless['LULC_ERODED'].copy()
+
+    assert not np.all(mask_before == mask_after)
+
+    for label in CLASSES:
+        if label == 0:
+            assert np.sum(mask_after == label) >= np.sum(mask_before == label), 'Error in the erosion process'
+        elif label in specific_labels:
+            assert np.sum(mask_after == label) <= np.sum(mask_before == label), 'Error in the erosion process'
+        else:
+            assert np.array_equal(mask_after == label, mask_before == label), 'Error in the erosion process'
