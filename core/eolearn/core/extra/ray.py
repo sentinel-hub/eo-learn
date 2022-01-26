256f687b02f23a0afdd789cfaa4e8c3a26e57037
maleksandrov <matej.aleksandrov@sinergise.com>
2022-01-26 10:28:08 +0100

    updated core requirements and types in Ray extension module

--- core/eolearn/core/extra/ray.py
+++ core/eolearn/core/extra/ray.py
@@ -9,25 +9,27 @@ Copyright (c) 2021-2021 Matej Aleksandrov, Žiga Lukšič (Sinergise)
 This source code is licensed under the MIT license found in the LICENSE
 file in the root directory of this source tree.
 """
+from typing import List
+
 try:
     import ray
 except ImportError as exception:
     raise ImportError('This module requires an installation of Ray Python package') from exception
 from tqdm.auto import tqdm
 
-from ..eoexecution import EOExecutor, _ProcessingType
+from ..eoexecution import EOExecutor, _ProcessingType, _ProcessingData
+from ..eoworkflow import WorkflowResults
 
 
 class RayExecutor(EOExecutor):
     """ A special type of `EOExecutor` that works with Ray framework
     """
-    def run(self):
+    def run(self) -> List[WorkflowResults]:
         """ Runs the executor using a Ray cluster
 
         Before calling this method make sure to initialize a Ray cluster using `ray.init`.
 
         :return: A list of EOWorkflow results
-        :rtype: None or list(eolearn.core.WorkflowResults)
         """
         if not ray.is_initialized():
             raise RuntimeError('Please initialize a Ray cluster before calling this method')
@@ -36,16 +38,16 @@ class RayExecutor(EOExecutor):
         return super().run(workers=workers, multiprocess=True)
 
     @staticmethod
-    def _get_processing_type(*_, **__):
+    def _get_processing_type(*_, **__) -> _ProcessingType:
         """ Provides a type of processing for later references
         """
         return _ProcessingType.RAY
 
     @classmethod
-    def _run_execution(cls, execution_kwargs, *_, **__):
+    def _run_execution(cls, processing_args: List[_ProcessingData], *_, **__) -> List[WorkflowResults]:
         """ Runs ray execution
         """
-        futures = [_ray_workflow_executor.remote(workflow_kwargs) for workflow_kwargs in execution_kwargs]
+        futures = [_ray_workflow_executor.remote(workflow_args) for workflow_args in processing_args]
 
         for _ in tqdm(_progress_bar_iterator(futures), total=len(futures)):
             pass
@@ -54,14 +56,14 @@ class RayExecutor(EOExecutor):
 
 
 @ray.remote
-def _ray_workflow_executor(workflow_kwargs):
+def _ray_workflow_executor(workflow_args: _ProcessingData) -> WorkflowResults:
     """ Called to execute a workflow on a ray worker
     """
     # pylint: disable=protected-access
-    return RayExecutor._execute_workflow(workflow_kwargs)
+    return RayExecutor._execute_workflow(workflow_args)
 
 
-def _progress_bar_iterator(futures):
+def _progress_bar_iterator(futures: object):
     """ A utility to help tracking finished ray processes
 
     Note that using tqdm(futures) directly would cause memory problems and is not accurate
