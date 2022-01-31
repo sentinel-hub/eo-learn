4ec37ae9de56198f7ebbad52c7bc1445b30fc67c
maleksandrov <matej.aleksandrov@sinergise.com>
2022-01-31 21:56:21 +0100

    removed AddValidDataMaskTask

--- mask/eolearn/mask/__init__.py
+++ mask/eolearn/mask/__init__.py
@@ -3,7 +3,7 @@ Public classes and functions of mask subpackage
 """
 
 from .cloud_mask import CloudMaskTask
-from .masking import AddValidDataMaskTask, MaskFeatureTask, JoinMasksTask
+from .masking import MaskFeatureTask, JoinMasksTask
 from .snow_mask import SnowMaskTask, TheiaSnowMaskTask
 from .utilities import resize_images
 from .mask_counting import ClassFrequencyTask
--- mask/eolearn/mask/masking.py
+++ mask/eolearn/mask/masking.py
@@ -15,39 +15,7 @@ from typing import Union, Callable
 
 import numpy as np
 
-from eolearn.core import EOTask, FeatureType, ZipFeatureTask
-
-
-class AddValidDataMaskTask(EOTask):
-    """EOTask for adding custom mask array used to filter reflectances data
-
-    This task allows the user to specify the criteria used to generate a valid data mask, which can be used to
-    filter the data stored in the `FeatureType.DATA`
-    """
-
-    def __init__(self, predicate, valid_data_feature=(FeatureType.MASK, "VALID_DATA")):
-        """Constructor of the class requires a predicate defining the function used to generate the valid data mask. A
-        predicate is a function that returns the truth value of some condition.
-
-        An example predicate could be an `and` operator between a cloud mask and a snow mask.
-
-        :param predicate: Function used to generate a `valid_data` mask
-        :type predicate: func
-        :param valid_data_feature: Feature which will store valid data mask
-        :type valid_data_feature: str
-        """
-        self.predicate = predicate
-        self.valid_data_feature = self.parse_feature(valid_data_feature)
-
-    def execute(self, eopatch):
-        """Execute predicate on input eopatch
-
-        :param eopatch: Input `eopatch` instance
-        :return: The same `eopatch` instance with a `mask.valid_data` array computed according to the predicate
-        """
-        feature_type, feature_name = next(self.valid_data_feature())
-        eopatch[feature_type][feature_name] = self.predicate(eopatch)
-        return eopatch
+from eolearn.core import EOTask, ZipFeatureTask
 
 
 class JoinMasksTask(ZipFeatureTask):
