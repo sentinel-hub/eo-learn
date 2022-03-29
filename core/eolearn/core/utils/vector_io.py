80561f64e343431f6cb5ec0008e9837571bcf63c
Ziga Luksic <ziga.luksic@live.com>
2022-03-29 11:16:24 +0200

    tell pylint to skip the vector_io file

--- core/eolearn/core/utils/vector_io.py
+++ core/eolearn/core/utils/vector_io.py
@@ -7,7 +7,7 @@ Copyright (c) 2017-2022 Matej Aleksandrov, Žiga Lukšič (Sinergise)
 This source code is licensed under the MIT license found in the LICENSE
 file in the root directory of this source tree.
 """
-
+# pylint: skip-file
 import numpy as np
 from geopandas.io.file import _geometry_types
 
