19059fca378324697bc050d4f57fc721c66e8f90
maleksandrov <matej.aleksandrov@sinergise.com>
2022-05-17 12:30:31 +0200

    added most useful parallelization functions to init

--- core/eolearn/core/__init__.py
+++ core/eolearn/core/__init__.py
@@ -28,7 +28,7 @@ from .eoworkflow import EOWorkflow, WorkflowResults
 from .eoworkflow_tasks import OutputTask
 from .utils.common import constant_pad, deep_eq
 from .utils.fs import get_filesystem, load_s3_filesystem
-from .utils.parallelize import execute_with_mp_lock
+from .utils.parallelize import execute_with_mp_lock, join_futures, join_futures_iter, parallelize
 from .utils.parsing import FeatureParser
 
 __version__ = "1.0.2"
