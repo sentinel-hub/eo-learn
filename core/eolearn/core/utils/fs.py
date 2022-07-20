f48223192ed2bd16866147c5db544e1543adbe1f
maleksandrov <matej.aleksandrov@sinergise.com>
2022-07-20 13:14:05 +0200

    Added utilities for pickling and unpickling filesystem objects

--- core/eolearn/core/utils/fs.py
+++ core/eolearn/core/utils/fs.py
@@ -8,13 +8,17 @@ Copyright (c) 2017-2022 Žiga Lukšič, Devis Peressutti, Nejc Vesel, Jovan Viš
 This source code is licensed under the MIT license found in the LICENSE
 file in the root directory of this source tree.
 """
+import copy
 import os
+import pickle
+import threading
 from pathlib import Path, PurePath
 from typing import Any, Optional, Tuple, Union
 
 import fs
 from boto3 import Session
 from fs.base import FS
+from fs.memoryfs import MemoryFS
 from fs_s3fs import S3FS
 
 from sentinelhub import SHConfig
@@ -119,6 +123,53 @@ def get_aws_credentials(aws_profile: str, config: Optional[SHConfig] = None) -> 
     config.aws_secret_access_key = aws_credentials.secret_key or ""
     config.aws_session_token = aws_credentials.token or ""
     return config
+
+
+def pickle_fs(filesystem: FS) -> bytes:
+    """By default, filesystem objects cannot be pickled because they contain thread locks and similar unserializable
+    objects. This function removes the before pickling the filesystem object.
+
+    :param filesystem: A filesystem object to pickle.
+    :return: A pickle of a filesystem object in bytes.
+    """
+    if not isinstance(filesystem, FS):
+        raise ValueError(f"Given filesystem object should be an instance of a class derived from {FS}")
+
+    filesystem = copy.copy(filesystem)
+
+    # pylint: disable=protected-access
+    filesystem._lock = None  # type: ignore[assignment]
+    if isinstance(filesystem, S3FS):
+        filesystem._tlocal = None
+    if isinstance(filesystem, MemoryFS):
+        filesystem.root.lock = None
+
+    try:
+        return pickle.dumps(filesystem)
+    except TypeError as exception:
+        raise NotImplementedError(f"Serialization of {type(filesystem)} is not yet supported") from exception
+
+
+def unpickle_fs(pickled_filesystem: bytes) -> FS:
+    """An inverse function of `pickle_fs` function. After unpickling a filesystem object it adds back all unserializable
+    objects that were previously removed by `pickle_fs`.
+
+    :param pickled_filesystem: Bytes that represent a pickled filesystem object.
+    :return: Unpickled filesystem object.
+    """
+    filesystem = pickle.loads(pickled_filesystem)
+
+    if not isinstance(filesystem, FS):
+        raise ValueError
+
+    # pylint: disable=protected-access
+    filesystem._lock = threading.RLock()
+    if isinstance(filesystem, S3FS):
+        filesystem._tlocal = threading.local()
+    if isinstance(filesystem, MemoryFS):
+        filesystem.root.lock = threading.RLock()
+
+    return filesystem
 
 
 def get_full_path(filesystem: FS, relative_path: str) -> str:
