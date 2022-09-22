eaf5d4588c4bee5f8bb72b24ec054914c4407a42
Ziga Luksic <ziga.luksic@live.com>
2022-09-22 14:39:37 +0200

    add core.utils.types module

---
+++ core/eolearn/core/utils/types.py
@@ -0,0 +1,20 @@
+"""
+Types and type aliases used throughout the code.
+
+Credits:
+Copyright (c) 2022 Žiga Lukšič (Sinergise)
+
+This source code is licensed under the MIT license found in the LICENSE
+file in the root directory of this source tree.
+"""
+# pylint: disable=unused-import
+import sys
+
+if sys.version_info >= (3, 10):
+    from types import EllipsisType  # pylint: disable=ungrouped-imports
+else:
+    import builtins  # noqa: F401
+
+    from typing_extensions import TypeAlias
+
+    EllipsisType: TypeAlias = "builtins.ellipsis"
