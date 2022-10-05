6da389f783693f5461817ce4bd5cf4ae64a1f99c
Ziga Luksic <ziga.luksic@live.com>
2022-10-05 09:32:25 +0200

    delay annotations in core_tasks for docbuilding purposes

--- core/eolearn/core/core_tasks.py
+++ core/eolearn/core/core_tasks.py
@@ -11,6 +11,8 @@ Copyright (c) 2017-2019 Blaž Sovdat, Andrej Burja (Sinergise)
 This source code is licensed under the MIT license found in the LICENSE
 file in the root directory of this source tree.
 """
+from __future__ import annotations
+
 import copy
 from abc import ABCMeta
 from typing import Any, Callable, Dict, Iterable, List, Optional, Tuple, Union, cast
