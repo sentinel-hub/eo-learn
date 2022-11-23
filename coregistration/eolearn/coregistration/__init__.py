e8f228c1923e35804f298721ec95b90c674ced36
Žiga Lukšič <31988337+zigaLuksic@users.noreply.github.com>
2022-11-23 15:03:19 +0100

    increase version to 1.3.1 (#508)

--- coregistration/eolearn/coregistration/__init__.py
+++ coregistration/eolearn/coregistration/__init__.py
@@ -4,4 +4,4 @@ A collection of tools and EOTasks for image co-registration
 
 from .coregistration import ECCRegistrationTask, InterpolationType, PointBasedRegistrationTask, RegistrationTask
 
-__version__ = "1.3.0"
+__version__ = "1.3.1"
