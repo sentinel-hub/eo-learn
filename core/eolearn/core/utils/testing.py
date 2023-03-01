be6058cbdf2458ecb4edc40caf561d9383b8f68b
Matic Lubej <matic.lubej@sinergise.com>
2023-03-01 14:01:02 +0100

    refactor patch_generator functionality, simplify some things

--- core/eolearn/core/utils/testing.py
+++ core/eolearn/core/utils/testing.py
@@ -13,7 +13,7 @@ file in the root directory of this source tree.
 """
 import datetime as dt
 from dataclasses import dataclass, field
-from typing import List, Optional, Set, Tuple
+from typing import List, Optional, Tuple
 
 import numpy as np
 import pandas as pd
@@ -24,60 +24,60 @@ from ..constants import FeatureType
 from ..eodata import EOPatch
 from ..utils.parsing import FeatureParser
 
+DEFAULT_BBOX = BBox((0, 0, 100, 100), crs=CRS.UTM_33N)
+
 
 @dataclass
 class PatchGeneratorConfig:
     """Dataclass containing a more complex setup of the PatchGenerator class."""
 
-    timestamps_periods: int = 15
-    timestamps_range: Tuple[str, str] = ("2019-01-01", "2019-12-31")
-
-    max_integer_value: int = 256
-    raster_shape: Tuple[int, int] = (300, 400)
-    depth_range: Tuple[int, int] = (1, 5)
-
-    num_random_features: int = 5
-    allowed_feature_types: Set[FeatureType] = field(default_factory=set)
-
-    bbox: BBox = field(init=False, repr=False)
+    num_timestamps: int = 5
+    timestamps_range: Tuple[dt.datetime, dt.datetime] = (dt.datetime(2019, 1, 1), dt.datetime(2019, 12, 31))
     timestamps: List[dt.datetime] = field(init=False, repr=False)
 
+    num_random_features: int = 5
+    max_integer_value: int = 256
+    raster_shape: Tuple[int, int] = (98, 151)
+    depth_range: Tuple[int, int] = (1, 3)
+
     def __post_init__(self):
-        self.bbox = BBox((0, 0, *self.raster_shape), crs=CRS.UTM_33N)
-        self.timestamps = list(pd.date_range(*self.timestamps_range, periods=self.timestamps_periods).to_pydatetime())
-        if not self.allowed_feature_types:
-            self.allowed_feature_types = {ftype for ftype in FeatureType if ftype.is_raster()}
+        self.timestamps = list(pd.date_range(*self.timestamps_range, periods=self.num_timestamps).to_pydatetime())
 
 
 def patch_generator(
     features: Optional[List[Tuple[FeatureType, str]]] = None,
-    bbox: Optional[BBox] = None,
+    bbox: BBox = DEFAULT_BBOX,
     timestamps: Optional[List[dt.datetime]] = None,
     seed: int = 42,
     config: Optional[PatchGeneratorConfig] = None,
 ):
     """A class for generating EOPatches with dummy data."""
     config = config if config is not None else PatchGeneratorConfig()
-    parsed_features = FeatureParser(features or [], config.allowed_feature_types).get_features()
+    supported_feature_types = [ftype for ftype in FeatureType if ftype.is_raster()]
+    parsed_features = FeatureParser(features or [], supported_feature_types).get_features()
     rng = np.random.default_rng(seed)
 
-    bbox = bbox if bbox is not None else config.bbox
     timestamps = timestamps if timestamps is not None else config.timestamps
     patch = EOPatch(bbox=bbox, timestamps=timestamps)
 
-    # generate random features
-    feature_types = [ftype for ftype, _ in parsed_features]
-    feature_types.extend(rng.choice(np.array(config.allowed_feature_types), config.num_random_features))
-    for ftype in set(feature_types):
-        parsed_features.extend([(ftype, f"{ftype.value}{idx}") for idx in range(feature_types.count(ftype) + 1)])
-
     # fill eopatch with random data
+    # note: the patch generation functionality could be extended by generating extra random features
     for ftype, fname in parsed_features:
-        shape = [len(timestamps)] if ftype.is_temporal() else []  # timestamps
-        shape.extend(config.raster_shape if ftype.is_spatial() else [])  # height, width
-        shape.append(rng.integers(*config.depth_range))  # depth
-
-        data = rng.integers(config.max_integer_value, size=shape) if ftype.is_discrete() else rng.normal(size=shape)
-        patch[(ftype, fname)] = data
-
+        shape = _get_feature_shape(rng, ftype, timestamps, config)
+        patch[(ftype, fname)] = _generate_feature_data(rng, ftype, shape, config)
     return patch
+
+
+def _generate_feature_data(
+    rng: np.random.Generator, ftype: FeatureType, shape: Tuple[int, ...], config: PatchGeneratorConfig
+) -> np.ndarray:
+    if ftype.is_discrete():
+        return rng.integers(config.max_integer_value, size=shape)
+    return rng.normal(size=shape)
+
+
+def _get_feature_shape(
+    rng: np.random.Generator, ftype: FeatureType, timestamps: List[dt.datetime], config: PatchGeneratorConfig
+) -> Tuple[int, ...]:
+    t, h, w, d = len(timestamps), *config.raster_shape, rng.integers(*config.depth_range)
+    return (t, h, w, d) if ftype.is_temporal() else (h, w, d)
