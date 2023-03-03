1d95264df042152c537bdef22b524c015f99ef32
Ziga Luksic <ziga.luksic@live.com>
2023-03-03 16:12:49 +0100

    fix contains for meta features in canonical representation

--- core/eolearn/core/eodata.py
+++ core/eolearn/core/eodata.py
@@ -407,7 +407,7 @@ class EOPatch:
             return bool(self[key])
         if isinstance(key, tuple) and len(key) == 2:
             ftype, fname = key
-            return fname in self[ftype]
+            return (fname in self[ftype]) if isinstance(fname, str) else bool(self[ftype])
         raise ValueError(
             f"Membership checking is only implemented for elements of type `{FeatureType.__name__}` and for "
             "`(feature_type, feature_name)` pairs."
