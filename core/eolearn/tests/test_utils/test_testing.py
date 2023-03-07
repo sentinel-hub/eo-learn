ceeaf640a073c0fd90bbd9fe5f939713dd36a385
jgersak <jan.gersak@sinergise.com>
2023-03-07 09:16:04 +0100

    Update test
    reorder tests
    rename variable in test_generate_eopatch_config
    update test_generate_eopatch_data
    add testcase for 2 and 1 D data

--- core/eolearn/tests/test_utils/test_testing.py
+++ core/eolearn/tests/test_utils/test_testing.py
@@ -26,16 +26,23 @@ def test_generate_eopatch_set_bbox_timestamps() -> None:
 
 
 @pytest.mark.parametrize(
-    "feature",
+    "config",
     [
-        (FeatureType.META_INFO, "meta_info"),
-        (FeatureType.VECTOR, "vector"),
-        (FeatureType.VECTOR_TIMELESS, "vector_timeless"),
+        {"num_timestamps": 3, "max_integer_value": 1, "raster_shape": (1, 1), "depth_range": (1, 2)},
+        {"num_timestamps": 7, "max_integer_value": 333, "raster_shape": (3, 27), "depth_range": (5, 15)},
     ],
 )
-def test_generate_eopatch_fails(feature: FeatureSpec) -> None:
-    with pytest.raises(ValueError):
-        generate_eopatch(feature)
+def test_generate_eopatch_config(config: Dict[str, Any]) -> None:
+    mask_feature = (FeatureType.MASK, "mask")
+
+    patch = generate_eopatch(mask_feature, config=PatchGeneratorConfig(**config))
+
+    time, raster_y, raster_x, depth = patch[mask_feature].shape
+    assert time == config["num_timestamps"]
+    assert (raster_y, raster_x) == config["raster_shape"]
+    assert config["depth_range"][0] <= depth <= config["depth_range"][1]
+
+    assert np.max(patch[mask_feature]) < config["max_integer_value"]
 
 
 @pytest.mark.parametrize("seed", [0, 1, 42, 100])
@@ -81,8 +88,7 @@ class GenerateTestCase:
                     "exp_shape": (5, 98, 151, 2),
                     "exp_min": 0,
                     "exp_max": 255,
-                    "exp_mean": 127.38907960535208,
-                    "rel_delta": 0.0001,
+                    "exp_mean": 127.389,
                 }
             },
         ),
@@ -92,24 +98,39 @@ class GenerateTestCase:
             expected_statistics={
                 (FeatureType.DATA, "data"): {
                     "exp_shape": (5, 98, 151, 1),
-                    "exp_min": -4.030404248695601,
-                    "exp_max": 4.406353522522504,
-                    "exp_mean": -0.00515678644681796,
-                    "rel_delta": 0.0001,
+                    "exp_min": -4.030404,
+                    "exp_max": 4.406353,
+                    "exp_mean": -0.005156786,
                 },
                 (FeatureType.MASK_TIMELESS, "LULC"): {
                     "exp_shape": (98, 151, 2),
                     "exp_min": 0,
                     "exp_max": 255,
-                    "exp_mean": 127.03858629544533,
-                    "rel_delta": 0.0001,
+                    "exp_mean": 127.0385,
                 },
                 (FeatureType.MASK_TIMELESS, "IS_VALUE"): {
                     "exp_shape": (98, 151, 2),
                     "exp_min": 0,
                     "exp_max": 255,
-                    "exp_mean": 127.31409649952697,
-                    "rel_delta": 0.0001,
+                    "exp_mean": 127.3140,
+                },
+            },
+        ),
+        GenerateTestCase(
+            seed=100,
+            data_features=[(FeatureType.SCALAR, "scalar"), (FeatureType.SCALAR_TIMELESS, "scalar_timeless")],
+            expected_statistics={
+                (FeatureType.SCALAR, "scalar"): {
+                    "exp_shape": (5, 2),
+                    "exp_min": -0.9613,
+                    "exp_max": 2.24297,
+                    "exp_mean": 0.7223,
+                },
+                (FeatureType.SCALAR_TIMELESS, "scalar_timeless"): {
+                    "exp_shape": (2,),
+                    "exp_min": -0.611493,
+                    "exp_max": 0.0472111,
+                    "exp_mean": -0.28214,
                 },
             },
         ),
@@ -118,23 +139,17 @@ class GenerateTestCase:
 def test_generate_eopatch_data(test_case: GenerateTestCase) -> None:
     patch = generate_eopatch(test_case.data_features, seed=test_case.seed)
     for feature in parse_features(test_case.data_features):
-        assert_statistics_match(patch[feature], **test_case.expected_statistics[feature])
+        assert_statistics_match(patch[feature], **test_case.expected_statistics[feature], rel_delta=0.0001)
 
 
 @pytest.mark.parametrize(
-    "config",
+    "feature",
     [
-        {"num_timestamps": 3, "max_integer_value": 1, "raster_shape": (1, 1), "depth_range": (1, 2)},
-        {"num_timestamps": 7, "max_integer_value": 333, "raster_shape": (3, 27), "depth_range": (5, 15)},
+        (FeatureType.META_INFO, "meta_info"),
+        (FeatureType.VECTOR, "vector"),
+        (FeatureType.VECTOR_TIMELESS, "vector_timeless"),
     ],
 )
-def test_generate_eopatch_config(config: Dict[str, Any]) -> None:
-    mask_feature = (FeatureType.MASK, "mask")
-
-    patch = generate_eopatch(mask_feature, config=PatchGeneratorConfig(**config))
-
-    time, raster_x, raster_y, depth = patch[mask_feature].shape
-    assert time == config["num_timestamps"]
-    assert (raster_x, raster_y) == config["raster_shape"]
-    assert config["depth_range"][0] <= depth <= config["depth_range"][1]
-    assert np.max(patch[mask_feature]) < config["max_integer_value"]
+def test_generate_eopatch_fails(feature: FeatureSpec) -> None:
+    with pytest.raises(ValueError):
+        generate_eopatch(feature)
