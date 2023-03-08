4db2a6b914af5c9ad8511f2ccc69b8fd86a5b78a
mengel <m.engel@tum.de>
2023-03-08 14:45:23 +0100

    feature parsing; allowed feature types; removed runtimerrors; less complex -> flakes states still too complex

--- ml_tools/eolearn/ml_tools/TDigestTask.py
+++ ml_tools/eolearn/ml_tools/TDigestTask.py
@@ -8,11 +8,13 @@ This source code is licensed under the MIT license found in the LICENSE
 file in the root directory of this source tree.
 """
 from itertools import product
+from typing import Optional
 
 import numpy as np
 import tdigest as td
 
-from eolearn.core import EOTask
+from eolearn.core import EOPatch, EOTask, FeatureType
+from eolearn.core.types import FeaturesSpecification
 
 
 class TDigestTask(EOTask):
@@ -24,12 +26,16 @@ class TDigestTask(EOTask):
     That enables quantile based normalisation or statistical analysis of datasets larger than RAM in EO.
     """
 
-    def __init__(self, in_feature, out_feature, *args, mode=None, pixelwise=True, **kwargs):
+    def __init__(
+        self,
+        in_feature: FeaturesSpecification,
+        out_feature: FeaturesSpecification,
+        mode: Optional[str] = None,
+        pixelwise: Optional[bool] = None,
+    ):
         """
         :param in_feature: The input feature to compute the T-Digest representation for.
-        :type in_feature: (FeatureType, str)
         :param out_feature: The output feature where to save the T-Digest representation of the chosen feature.
-        :type out_feature: (FeatureType, str)
         :param mode: The mode to apply to the timestamps and bands.
         - The 'standard' mode computes the T-Digest representation \
             for each band accumulating timestamps.
@@ -37,81 +43,80 @@ class TDigestTask(EOTask):
             for each band and timestamp of the chosen feature.
         - The 'monthly' mode computes the T-Digest representation \
             for each band accumulating the timestamps per month.
-        - The 'weekly' mode computes the T-Digest representation \
-            for each band accumulating the timestamps per seven days.
-        - The 'daily' mode computes the T-Digest representation \
-            for each band accumulating the timestamps per day.
         - The 'total' mode computes the total T-Digest representation \
-            of the whole feature accumulating all timestamps, bands and pixels.
-        :type mode: str
-        :param pixelwise: Decider whether to compute the T-Digest representation accumulating pixels or per pixel.
-        :type pixelwise: boolean
-        :param args: arguments for init of EOTask base class.
-        :type args: arguments
-        :param kwargs: keyword arguments for init of EOTask base class.
-        :type kwargs: keyword arguments
+            of the whole feature accumulating all timestamps, bands and pixels \
+                - cannot be used with pixelwise=True.
+        :param pixelwise: Decider whether to compute the T-Digest representation accumulating pixels or per pixel. \
+            Cannot be used with mode='total'.
         """
-        super().__init__(*args, **kwargs)
-
-        # check input and output features
-        if isinstance(in_feature, list):
-            self.in_feature = in_feature
-            if isinstance(out_feature, list):
-                assert len(in_feature) == len(out_feature)
-                self.out_feature = out_feature
-            else:
-                raise RuntimeError(
-                    "TDigestTask: you cannot give a list of input features without specifying the a list of output"
-                    " features of the same length!"
-                )
-        else:
-            self.in_feature = [in_feature]
-            if isinstance(out_feature, list):
-                raise RuntimeError(
-                    "TDigestTask: you cannot get a list of T-Digest representations from a single feature!"
-                )
-            else:
-                self.out_feature = [out_feature]
 
         # check pixelwise parameter
-        if isinstance(pixelwise, list):
-            if len(pixelwise) == len(self.out_feature):
-                self.pixelwise = pixelwise
-            else:
-                assert len(pixelwise) == len(self.out_feature)
+        if not pixelwise:
+            self.pixelwise = False
+        elif pixelwise and self.mode == "total":
+            raise ValueError("Total mode does not support pixelwise=True.")
         else:
-            self.pixelwise = [pixelwise] * len(self.out_feature)
+            self.pixelwise = pixelwise
 
         # set mode
-        self.mode = mode
+        if not mode:
+            self.mode = "standard"
+        else:
+            self.mode = mode
 
-    def execute(self, eopatch):
+        # set feature types
+        if mode == "standard":
+            allowed_in_types = [
+                FeatureType.DATA,
+                FeatureType.DATA_TIMELESS,
+                FeatureType.MASK,
+                FeatureType.MASK_TIMELESS,
+            ]
+            if self.pixelwise:
+                allowed_out_types = [FeatureType.DATA_TIMELESS]
+            else:
+                allowed_out_types = [FeatureType.SCALAR_TIMELESS]
+
+        elif mode == "timewise" or mode == "monthly":
+            allowed_in_types = [FeatureType.DATA, FeatureType.MASK]
+            if self.pixelwise:
+                allowed_out_types = [FeatureType.DATA]
+            else:
+                allowed_out_types = [FeatureType.SCALAR]
+
+        elif mode == "total":
+            allowed_in_types = None
+            allowed_out_types = [FeatureType.SCALAR_TIMELESS]
+
+        else:
+            raise ValueError(f"The mode {mode} is not allowed.")
+
+        # check input and output features
+        self.in_feature = self.parse_features(in_feature, allowed_feature_types=allowed_in_types)
+        self.out_feature = self.parse_features(out_feature, allowed_feature_types=allowed_out_types)
+
+        if len(self.in_feature) != len(self.out_feature):
+            raise ValueError(
+                f"The number of input ({len(self.in_feature)}) and output features ({len(self.out_feature)}) must"
+                " match."
+            )
+
+    def execute(self, eopatch: EOPatch) -> EOPatch:
         """
         Execute method that computes the TDigest of the chosen features.
 
         :param eopatch: EOPatch which the chosen input feature already exists
-        :type eopatch: EOPatch
         """
 
         # standard mode
-        if self.mode is None or self.mode == "standard":
-            for in_feature, out_feature, pixelwise in zip(self.in_feature, self.out_feature, self.pixelwise):
+        if self.mode == "standard":
+            for in_feature, out_feature in zip(self.in_feature, self.out_feature):
                 shape = np.array(eopatch[in_feature].shape)
-                if pixelwise:
-                    eopatch[out_feature] = np.empty(shape[1:], dtype=object)
-                    if len(shape) == 4:  # add feature is temporal check
-                        for i, j, k in product(range(shape[1]), range(shape[2]), range(shape[3])):
-                            eopatch[out_feature][i, j, k] = td.TDigest()
-                            eopatch[out_feature][i, j, k].batch_update(eopatch[in_feature][:, i, j, k].flatten())
-                    elif len(shape) == 3:  # add feature is timeless check
-                        for i, j, k in product(range(shape[0]), range(shape[1]), range(shape[2])):
-                            eopatch[out_feature][i, j, k] = td.TDigest()
-                            eopatch[out_feature][i, j, k].batch_update(eopatch[in_feature][i, j, k].flatten())
-                    else:
-                        raise RuntimeError(
-                            f"TDigestTask: {self.mode} mode only defined for DATA, DATA_TIMELESS, MASK or MASK_TIMELESS"
-                            " feature types!"
-                        )
+                if self.pixelwise:
+                    eopatch[out_feature] = np.empty(shape[-3:], dtype=object)
+                    for i, j, k in product(range(shape[-3]), range(shape[-2]), range(shape[-1])):
+                        eopatch[out_feature][i, j, k] = td.TDigest()
+                        eopatch[out_feature][i, j, k].batch_update(eopatch[in_feature][..., i, j, k].flatten())
 
                 else:
                     eopatch[out_feature] = np.empty(shape[-1], dtype=object)
@@ -121,31 +126,19 @@ class TDigestTask(EOTask):
 
         # timewise mode
         elif self.mode == "timewise":
-            for time_, timestamp in enumerate(eopatch["timestamp"]):
-                for in_feature, out_feature, pixelwise in zip(self.in_feature, self.out_feature, self.pixelwise):
-                    out_feature_type, out_feature_name = out_feature
-                    out_feature = (out_feature_type, out_feature_name + f"_{timestamp.strftime('%Y-%m-%d-%H-%M-%S')}")
+            for in_feature, out_feature in zip(self.in_feature, self.out_feature):
+                shape = np.array(eopatch[in_feature].shape)
+                if self.pixelwise:
+                    eopatch[out_feature] = np.empty(shape, dtype=object)
+                    for time_, i, j, k in product(range(shape[0]), range(shape[1]), range(shape[2]), range(shape[3])):
+                        eopatch[out_feature][time_, i, j, k] = td.TDigest()
+                        eopatch[out_feature][time_, i, j, k].batch_update(eopatch[in_feature][time_, i, j, k].flatten())
 
-                    shape = np.array(eopatch[in_feature].shape)
-                    if pixelwise:
-                        eopatch[out_feature] = np.empty(shape, dtype=object)
-                        if len(shape) == 4:  # add feature is temporal check
-                            for i, j, k in product(range(shape[1]), range(shape[2]), range(shape[3])):
-                                eopatch[out_feature][time_, i, j, k] = td.TDigest()
-                                eopatch[out_feature][time_, i, j, k].batch_update(
-                                    eopatch[in_feature][time_, i, j, k].flatten()
-                                )
-                        else:
-                            raise RuntimeError(
-                                f"TDigestTask: {self.mode} mode only defined for DATA or MASK feature types with time"
-                                " component!"
-                            )
-
-                    else:
-                        eopatch[out_feature] = np.empty(shape[[0, -1]], dtype=object)
-                        for k in range(shape[-1]):
-                            eopatch[out_feature][time_, k] = td.TDigest()
-                            eopatch[out_feature][time_, k].batch_update(eopatch[in_feature][time_, ..., k].flatten())
+                else:
+                    eopatch[out_feature] = np.empty(shape[[0, -1]], dtype=object)
+                    for k in range(shape[-1]):
+                        eopatch[out_feature][time_, k] = td.TDigest()
+                        eopatch[out_feature][time_, k].batch_update(eopatch[in_feature][time_, ..., k].flatten())
 
         # monthly mode
         elif self.mode == "monthly":
@@ -153,20 +146,14 @@ class TDigestTask(EOTask):
             for month_ in range(12):
                 midx.append(np.array([timestamp.month == month_ + 1 for timestamp in eopatch["timestamp"]]))
 
-            for in_feature, out_feature, pixelwise in zip(self.in_feature, self.out_feature, self.pixelwise):
+            for in_feature, out_feature in zip(self.in_feature, self.out_feature):
                 shape = np.array(eopatch[in_feature].shape)
-                if pixelwise:
+                if self.pixelwise:
                     eopatch[out_feature] = np.empty([12, *shape[1:]], dtype=object)
-                    if len(shape) == 4:  # add feature is temporal check
-                        for month_, i, j, k in product(range(12), range(shape[1]), range(shape[2]), range(shape[3])):
-                            eopatch[out_feature][month_, i, j, k] = td.TDigest()
-                            eopatch[out_feature][month_, i, j, k].batch_update(
-                                eopatch[in_feature][midx[month_], i, j, k].flatten()
-                            )
-                    else:
-                        raise RuntimeError(
-                            f"TDigestTask: {self.mode} mode only defined for DATA or MASK feature types with time"
-                            " component!"
+                    for month_, i, j, k in product(range(12), range(shape[1]), range(shape[2]), range(shape[3])):
+                        eopatch[out_feature][month_, i, j, k] = td.TDigest()
+                        eopatch[out_feature][month_, i, j, k].batch_update(
+                            eopatch[in_feature][midx[month_], i, j, k].flatten()
                         )
 
                 else:
@@ -177,19 +164,11 @@ class TDigestTask(EOTask):
                             eopatch[in_feature][midx[month_], ..., k].flatten()
                         )
 
-        # to be implemented modes
-        elif self.mode == "weekly" or self.mode == "daily":
-            raise NotImplementedError(f"TDigestTask: {self.mode} mode not implemented yet!")
-
         # total mode
         elif self.mode == "total":
-            for in_feature, out_feature, pixelwise in zip(self.in_feature, self.out_feature, self.pixelwise):
-                if pixelwise:
-                    raise NotImplementedError(f"TDigestTask: pixelwise for {self.mode} mode not implemented yet!")
-
-                else:
-                    eopatch[out_feature] = td.TDigest()
-                    eopatch[out_feature].batch_update(eopatch[in_feature].flatten())
+            for in_feature, out_feature in zip(self.in_feature, self.out_feature):
+                eopatch[out_feature] = td.TDigest()
+                eopatch[out_feature].batch_update(eopatch[in_feature].flatten())
 
         # errorneous modes
         else:
