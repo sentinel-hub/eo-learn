c3362283fce81437bce113b6a6145fc6e9d86650
Matic Lubej <matic.lubej@sinergise.com>
2023-03-10 11:03:05 +0100

    use official way of deprecation

--- mask/eolearn/mask/utils.py
+++ mask/eolearn/mask/utils.py
@@ -11,6 +11,10 @@ from typing import Callable
 import cv2
 import numpy as np
 
+from sentinelhub.exceptions import deprecated_function
+
+from eolearn.core.exceptions import EODeprecationWarning
+
 
 def map_over_axis(data: np.ndarray, func: Callable[[np.ndarray], np.ndarray], axis: int = 0) -> np.ndarray:
     """Map function func over each slice along axis.
@@ -41,15 +45,10 @@ def map_over_axis(data: np.ndarray, func: Callable[[np.ndarray], np.ndarray], ax
     return mapped_data
 
 
-def resize_images(  # type: ignore[no-untyped-def]
-    data,
-    new_size=None,
-    scale_factors=None,
-    anti_alias=True,
-    interpolation="linear",
-):
-    """DEPRECATED, please use `eolearn.features.utils.spatially_resize_image` instead.
-
+@deprecated_function(EODeprecationWarning, "Please use `eolearn.features.utils.spatially_resize_image` instead.")
+# type: ignore[no-untyped-def]
+def resize_images(data, new_size=None, scale_factors=None, anti_alias=True, interpolation="linear"):
+    """
     Resizes the image(s) according to given size or scale factors.
 
     To specify the new scale use one of `new_size` or `scale_factors` parameters.
