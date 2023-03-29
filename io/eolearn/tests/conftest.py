d9f1ed80e207fc95c45c7f973b74471ee20a07c7
Matic Lubej <matic.lubej@sinergise.com>
2023-03-29 22:09:35 +0200

    fix SHConfig fixture so that tests work

--- io/eolearn/tests/conftest.py
+++ io/eolearn/tests/conftest.py
@@ -33,7 +33,7 @@ def example_data_path_fixture():
 @pytest.fixture(name="config")
 def config_fixture():
     config = SHConfig()
-    for param in config.get_params():
+    for param in config.to_dict():
         env_variable = param.upper()
         if os.environ.get(env_variable):
             setattr(config, param, os.environ.get(env_variable))
