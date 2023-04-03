951eeb8c7c80c08268b6e88cdeb6f825c6b5f316
mengel <m.engel@tum.de>
2023-04-03 14:30:11 +0200

    fixed pylint issues

--- ml_tools/eolearn/ml_tools/TDigestTask.py
+++ ml_tools/eolearn/ml_tools/TDigestTask.py
@@ -114,36 +114,36 @@ def _looper(in_feature: FeaturesSpecification, out_feature: FeaturesSpecificatio
         yield in_feature_, out_feature_, shape
 
 
-def _process_standard(input_array: np.ndarray, shape: np.ndarray, pixelwise: bool, **kwargs) -> np.ndarray:
+def _process_standard(input_array: np.ndarray, shape: np.ndarray, pixelwise: bool, **_) -> np.ndarray:
     if pixelwise:
         array = np.empty(shape[-3:], dtype=object)
         for i, j, k in product(range(shape[-3]), range(shape[-2]), range(shape[-1])):
-            array[i, j, k] = get_tdigest(input_array[..., i, j, k])
+            array[i, j, k] = _get_tdigest(input_array[..., i, j, k])
 
     else:
         array = np.empty(shape[-1], dtype=object)
         for k in range(shape[-1]):
-            array[k] = get_tdigest(input_array[..., k])
+            array[k] = _get_tdigest(input_array[..., k])
 
     return array
 
 
-def _process_timewise(input_array: np.ndarray, shape: np.ndarray, pixelwise: bool, **kwargs) -> np.ndarray:
+def _process_timewise(input_array: np.ndarray, shape: np.ndarray, pixelwise: bool, **_) -> np.ndarray:
     if pixelwise:
         array = np.empty(shape, dtype=object)
         for time_, i, j, k in product(range(shape[0]), range(shape[1]), range(shape[2]), range(shape[3])):
-            array[time_, i, j, k] = get_tdigest(input_array[time_, i, j, k])
+            array[time_, i, j, k] = _get_tdigest(input_array[time_, i, j, k])
 
     else:
         array = np.empty(shape[[0, -1]], dtype=object)
         for time_, k in product(range(shape[0]), range(shape[-1])):
-            array[time_, k] = get_tdigest(input_array[time_, ..., k])
+            array[time_, k] = _get_tdigest(input_array[time_, ..., k])
 
     return array
 
 
 def _process_monthly(
-    input_array: np.ndarray, timestamps: Iterable, shape: np.ndarray, pixelwise: bool, **kwargs
+    input_array: np.ndarray, timestamps: Iterable, shape: np.ndarray, pixelwise: bool, **_
 ) -> np.ndarray:
     midx = []
     for month_ in range(12):
@@ -152,18 +152,18 @@ def _process_monthly(
     if pixelwise:
         array = np.empty([12, *shape[1:]], dtype=object)
         for month_, i, j, k in product(range(12), range(shape[1]), range(shape[2]), range(shape[3])):
-            array[month_, i, j, k] = get_tdigest(input_array[midx[month_], i, j, k])
+            array[month_, i, j, k] = _get_tdigest(input_array[midx[month_], i, j, k])
 
     else:
         array = np.empty([12, shape[-1]], dtype=object)
         for month_, k in product(range(12), range(shape[-1])):
-            array[month_, k] = get_tdigest(input_array[midx[month_], ..., k])
+            array[month_, k] = _get_tdigest(input_array[midx[month_], ..., k])
 
     return array
 
 
-def _process_total(input_array: np.ndarray, **kwargs) -> np.ndarray:
-    return get_tdigest(input_array)
+def _process_total(input_array: np.ndarray, **_) -> np.ndarray:
+    return _get_tdigest(input_array)
 
 
 _processing_function = {
@@ -174,7 +174,7 @@ _processing_function = {
 }
 
 
-def get_tdigest(values: np.ndarray) -> td.TDigest:
+def _get_tdigest(values: np.ndarray) -> td.TDigest:
     result = td.TDigest()
     result.batch_update(values.flatten())
     return result
