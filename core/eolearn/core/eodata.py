f088922468267b168fb8542fdc1067825e77f2f8
Ziga Luksic <ziga.luksic@live.com>
2023-06-20 16:04:54 +0200

    apply linters

--- core/eolearn/core/eodata.py
+++ core/eolearn/core/eodata.py
@@ -292,7 +292,9 @@ class EOPatch:
         else:
             raise TypeError(f"Cannot assign {value} as timestamps. Should be a sequence of datetime.datetime objects.")
 
-    def get_timestamps(self, message_on_failure: str = "This EOPatch does not contain timestamps.") -> list[dt.datetime]:
+    def get_timestamps(
+        self, message_on_failure: str = "This EOPatch does not contain timestamps."
+    ) -> list[dt.datetime]:
         """Returns the `timestamps` attribute if the EOPatch is temporally defined. Fails otherwise."""
         if self._timestamps is None:
             raise RuntimeError(message_on_failure)
