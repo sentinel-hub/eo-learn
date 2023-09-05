9a40958f3ad032f04647392abe292ac0ba61a535
Ziga Luksic <ziga.luksic@live.com>
2023-09-05 11:44:11 +0200

    clean geometry and mask

--- eolearn/geometry/__init__.py
+++ eolearn/geometry/__init__.py
@@ -3,10 +3,4 @@ Subpackage containing EOTasks for geometrical transformations
 """
 
 from .morphology import ErosionTask, MorphologicalFilterTask, MorphologicalOperations, MorphologicalStructFactory
-from .superpixel import (
-    FelzenszwalbSegmentationTask,
-    MarkSegmentationBoundariesTask,
-    SlicSegmentationTask,
-    SuperpixelSegmentationTask,
-)
 from .transformations import RasterToVectorTask, VectorToRasterTask
