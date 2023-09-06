d9dcffe6c10f4f6ac3bd29c50f300a8fa32b92ee
Matic Lubej <matic.lubej@sinergise.com>
2023-09-06 16:26:59 +0200

    Merge separate extras into a single one (#753)
    * avoid underscore in extras
    
    * switch to single extra extra
    
    ---------
    
    Co-authored-by: Ziga Luksic <ziga.luksic@live.com>

--- eolearn/mask/extra/__init__.py
+++ eolearn/mask/extra/__init__.py
@@ -1,4 +1,4 @@
 """
-A collection of EOTasks with non-standard dependencies. Use the extra `MASK_EXTRA` to install dependencies or
+A collection of EOTasks with non-standard dependencies. Use the extra `[EXTRA]` to install dependencies or
 install the module-specific dependencies by hand.
 """
