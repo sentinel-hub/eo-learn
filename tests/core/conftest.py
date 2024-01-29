88ce0bb1605b02ef5c5fba29de2630d8e8f71429
Žiga Lukšič <31988337+zigaLuksic@users.noreply.github.com>
2024-01-29 13:07:57 +0100

    Adjust moto and lazy fixture after recent updates (#786)
    * general changes
    
    * fix remaining problems

--- tests/core/conftest.py
+++ tests/core/conftest.py
@@ -10,12 +10,8 @@ This source code is licensed under the MIT license, see the LICENSE file in the 
 from __future__ import annotations
 
 import os
-from typing import Callable
 
-import boto3
 import pytest
-from fs_s3fs import S3FS
-from moto import mock_s3
 
 from eolearn.core import EOPatch
 
@@ -28,26 +24,3 @@ def test_eopatch_path_fixture() -> str:
 @pytest.fixture(name="test_eopatch")
 def test_eopatch_fixture(test_eopatch_path) -> EOPatch:
     return EOPatch.load(test_eopatch_path)
-
-
-@pytest.fixture(name="create_mocked_s3fs", scope="session")
-def s3_mocking_fixture() -> Callable[[str], S3FS]:
-    """Provides a function for mocking S3 buckets"""
-
-    @mock_s3
-    def create_mocked_s3fs(bucket_name: str = "mocked-test-bucket") -> S3FS:
-        """Creates a new empty mocked s3 bucket. If one such bucket already exists it deletes it first."""
-        s3resource = boto3.resource("s3", region_name="eu-central-1")
-
-        bucket = s3resource.Bucket(bucket_name)
-
-        if bucket.creation_date:  # If bucket already exists
-            for key in bucket.objects.all():
-                key.delete()
-            bucket.delete()
-
-        s3resource.create_bucket(Bucket=bucket_name, CreateBucketConfiguration={"LocationConstraint": "eu-central-1"})
-
-        return S3FS(bucket_name=bucket_name)
-
-    return create_mocked_s3fs
